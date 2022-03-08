e3471e5b77f79cd32de023a9958109c1f8f2039d
ByeongGwon <lee971009@naver.com>
2022-03-08 23:51:07 +0900

    주석 추가

--- 1247.py
+++ 1247.py
@@ -1,11 +1,12 @@
-from sys import stdin
+from sys import stdin #input대신 사용하는 용도, 속도가 더 빠름
 
-for _ in range(3) : 
-  N = int(stdin.readline());
-  total = 0;
+for _ in range(3) :  # 3번의 테스트셋을 받음
+  N = int(stdin.readline()); #테스트 개수 받아옴
+  total = 0; 
   for i in range(N):
-    b = int(stdin.readline());
+    b = int(stdin.readline()); #테스트 값
     total += b
+    
   if(total > 0) :
     print("+")
   elif (total < 0) :
