e167d1940632970013c72df8cc5fc4d418cdda88
ByeongGwon <lee971009@naver.com>
2022-03-09 13:30:16 +0900

    6개

---
+++ 1010.py
@@ -0,0 +1,18 @@
+# 콤비네이션 하면 됨.
+
+from sys import stdin
+from itertools import combinations
+import math
+
+
+number = int(stdin.readline())
+
+for i in range(number):
+    n, m = map(int, stdin.readline().split())
+    # print(f'n = {n} , m = {m}')
+    it = int(math.factorial(m) / (math.factorial(n) * math.factorial(m-n)))
+    print(it)
+    # print(combinations(range(1, n), m))
+# 콤비네이션으로 하면 더 느림
+
+# print(math.factorial(29) / math.factorial(13) * math.factorial(16))
