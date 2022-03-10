0fd2b541a03dd7b9ef10d6a3ad4a6d0a1feca86a
ByeongGwon <lee971009@naver.com>
2022-03-10 21:33:15 +0900

    silver 4

---
+++ 1065.py
@@ -0,0 +1,27 @@
+n = int(input())
+
+count = 0
+
+for i in range(1, n+1):
+    a = i // 100
+    b = (i % 100) // 10
+    c = (i % 10)
+
+    if(i < 10):
+        count += 1
+        # print(f"10까지 Count = {count}")
+    elif(i < 100):
+        count += 1
+        # print(f"100까지 I = {i}, Count={count}")
+    else:
+        if(((b-a == c-b) or (a-b == b-c))):
+            count += 1
+
+print(count)
+# i = 11
+
+# a = i // 100
+# b = (i % 100) // 10
+# c = (i % 10)
+
+print(a, b, c)
