a6dcf968a49efc74b81e75b49f1f38329a4b942e
ByeongGwon <lee971009@naver.com>
2022-03-11 20:14:15 +0900

    Silver 4

--- 1978.py
+++ 1978.py
@@ -7,3 +7,14 @@ for i in range(2, n+1):
         primes.append(i)
         for j in range(2*i, n+1, i):
             a[j] = False
+
+n = int(input())
+count = 0
+li = []
+lit = list(map(int, input().split()))
+li += lit
+for x in li:
+    if(x in primes):
+        count += 1
+
+print(count)
