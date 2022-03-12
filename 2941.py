2ad3059b942cb498d7ddac3f53407c65cb4ff957
ByeongGwon <lee971009@naver.com>
2022-03-12 20:20:41 +0900

    Silver 4

--- 2941.py
+++ 2941.py
@@ -2,6 +2,6 @@ a = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 alpha = input()
 for t in a:
     alpha = alpha.replace(t, '*')
-    print(f"alpha change t='{t}' alpha = {alpha}")
-print(alpha)
+    # print(f"alpha change t='{t}' alpha = {alpha}")
+# print(alpha)
 print(len(alpha))
