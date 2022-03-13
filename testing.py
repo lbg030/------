3b6b35ba7b7d2f04579f647b52b0c7abebaf90ba
ByeongGwon <lee971009@naver.com>
2022-03-13 11:57:10 +0900

    Silver3

--- testing.py
+++ testing.py
@@ -1,5 +1,4 @@
-n, m = map(int, input().split())
-n = [i for i in range(1, n+1)]
-li = list(map(int, input().split()))
-todo = 0
-print(n.index(li[todo]) < (len(n) // 2))
+li = [1, 2]
+\
+
+print(li[-2])
