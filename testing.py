b80d93e848c990e8071d80301d3a2df61bb61488
ByeongGwon <lee971009@naver.com>
2022-03-13 16:23:36 +0900

    Silver 4

--- testing.py
+++ testing.py
@@ -1,1 +1,20 @@
-print(6 % 3)
+sen = "So when I die (the [first] I will see in (heaven) is a score list)."
+
+leftsmall = 0
+rightsmall = 0
+leftbig = 0
+rightbig = 0
+
+for j in range(len(sen)):
+    if('(' == sen[j]):
+        leftsmall += 1
+    if('[' == sen[j]):
+        leftbig += 1
+    if(')' == sen[j]):
+        rightsmall += 1
+    if(']' == sen[j]):
+        rightbig += 1
+
+
+print(
+    f"번째 leftsmall = {leftsmall}, rightsmall = {rightsmall} leftbig = {leftbig} rightbig = {rightbig}")
