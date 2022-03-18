c19dbcde9c449c4f41194758ae3fa4324d6cad57
ByeongGwon <lee971009@naver.com>
2022-03-18 10:45:08 +0900

    Silver 4

---
+++ 10845.py
@@ -0,0 +1,37 @@
+
+from collections import deque
+from sys import stdin
+
+Stack = deque()
+
+for _ in range(int(input())):
+    m = stdin.readline().split()
+    # print(m)
+
+    if(m[0] == 'push'):
+        Stack.append(int(m[1]))
+
+    elif(m[0] == 'pop'):
+        if(len(Stack) > 0):
+            pop_entry = Stack.popleft()
+            print(pop_entry)
+        else:
+            print('-1')
+    elif(m[0] == 'back'):
+        if(len(Stack) > 0):
+            print(Stack[-1])
+        else:
+            print('-1')
+
+    elif(m[0] == 'empty'):
+        if(len(Stack) == 0):
+            print('1')
+        else:
+            print('0')
+    elif(m[0] == 'front'):
+        if(len(Stack) == 0):
+            print('-1')
+        else:
+            print(Stack[0])
+    elif(m[0] == 'size'):
+        print(len(Stack))
