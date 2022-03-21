57c3489c8f58b5a85cc83fa4c54e501578a0297c
ByeongGwon <lee971009@naver.com>
2022-03-21 11:35:20 +0900

    Silver 2

---
+++ 11053.py
@@ -0,0 +1,12 @@
+n = int(input())
+li = list(map(int, input().split()))
+dp = [1] * n
+# print(li)
+for i in range(n):
+    for j in range(i):
+        if(li[i] > li[j]):
+            # print(f"dp = {li[j]}, i = {i} j = {dp[j]}")
+            dp[i] = max(dp[i], dp[j]+1)
+            # print(dp[i])
+
+print(max(dp))
