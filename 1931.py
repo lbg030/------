68cbb32ff7a282160fe46e1fd71908318af23380
ByeongGwon <lee971009@naver.com>
2022-03-25 15:30:02 +0900

    Silver 2

--- 1931.py
+++ 1931.py
@@ -1,8 +1,15 @@
 n = int(input())
-
-li = []
-for _ in range(n):
-    li.append(list(map(int, input().split())))
-
-li.sort()
-print(li)
+s = []
+for i in range(n):
+    first, second = map(int, input().split())
+    s.append([first, second])
+s = sorted(s, key=lambda a: a[0])
+s = sorted(s, key=lambda a: a[1])
+last = 0
+cnt = 0
+print(s)
+for i, j in s:
+    if i >= last:
+        cnt += 1
+        last = j
+print(cnt)
