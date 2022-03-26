e59561711025b1f6eeccb1a1f3c393866fce932c
ByeongGwon <lee971009@naver.com>
2022-03-26 17:25:47 +0900

    Silver 2

---
+++ 11279.py
@@ -0,0 +1,16 @@
+import heapq
+from sys import stdin
+n = int(input())
+
+lst = []
+
+for _ in range(n):
+    n = int(stdin.readline())
+    if(n == 0):
+        if(len(lst) == 0):
+            print("0")
+        else:
+            print(-heapq.heappop(lst))
+
+    else:
+        heapq.heappush(lst, -n)
