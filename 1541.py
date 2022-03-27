ec04754b2151c3ee6dde5451eef182140c583443
ByeongGwon <lee971009@naver.com>
2022-03-27 15:53:35 +0900

    Silver 2

---
+++ 1541.py
@@ -0,0 +1,13 @@
+n = list(input().split('-'))
+
+# print(n)
+
+n = [ele.lstrip('0') for ele in n]
+
+print(n)
+# result = eval(n[0])
+
+# for x in n:
+#     result -= eval(x)
+
+# print(result + eval(n[0]))
