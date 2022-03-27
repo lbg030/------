0266babd8b73548d65ba882d152357c62c5e344d
ByeongGwon <lee971009@naver.com>
2022-03-27 14:37:19 +0900

    Bronze 4

--- 10162.py
+++ 10162.py
@@ -1,6 +1,15 @@
 n = int(input())
 
 li = [300, 60, 10]
-
+res = []
+# 나누어 떨어지지 않는 경우
 if(n % li[2] != 0):
     print("-1")
+else:
+    i = 0
+    while i < 3:
+        res.append(n // li[i])
+        n = n % li[i]
+        i += 1
+
+    print(*res)
