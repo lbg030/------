c2e79076912d77d115455e768e095306184a6285
ByeongGwon <lee971009@naver.com>
2022-03-27 15:32:56 +0900

    Silver 4

--- 2217.py
+++ 2217.py
@@ -4,4 +4,13 @@ n = int(input())
 li = []
 for _ in range(n):
     li.append(int(stdin.readline().rstrip()))
-print(li)
+
+li.sort(reverse=True)
+result = 0
+# print(li)
+for i in range(0, n):
+    result = max(result, li[i]*(i+1))
+
+print(result)
+# 27 26 25 가 존재
+# 27, 26.5, 25.5
