edfbdf3b4cb49a6a4443015e3a66124de07e6dec
ByeongGwon <lee971009@naver.com>
2022-03-27 15:06:12 +0900

    Silver 5

--- 10610.py
+++ 10610.py
@@ -1,3 +1,13 @@
 from sys import stdin
 
-n = list(stdin.readline())
+n = list(stdin.readline().rstrip())
+
+n = sorted(n, reverse=True)
+
+n = list(map(int, n))
+# print(n)
+
+if(0 not in n or sum(n) % 3 != 0):
+    print("-1")
+else:
+    print(*n, sep='')
