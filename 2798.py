acd7ac590124d69cbb104e0e218f2964af8084d1
ByeongGwon <lee971009@naver.com>
2022-03-28 09:01:51 +0900

    Bronze 2

--- 2798.py
+++ 2798.py
@@ -1,7 +1,14 @@
 from sys import stdin
-
+from itertools import combinations
 n, m = map(int, input().split())
 
-li = list(map(int, stdin.readline().split()))
+li = sorted(list(map(int, stdin.readline().split())), reverse=True)
+# print(li)
+total = 0
 
-print(li)
+li2 = list(combinations(li, 3))
+for x in li2:
+    if(sum(x) <= m):
+        total = max(sum(x), total)
+
+print(total)
