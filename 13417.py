4996eba80ebb5930438955c7d2a4e835066cc4ff
ByeongGwon <lee971009@naver.com>
2022-04-02 20:12:52 +0900

    Silver 2

--- 13417.py
+++ 13417.py
@@ -1,3 +1,10 @@
+from sys import stdin
+from collections import deque
+
 n = int(input())
 for _ in range(n):
     m = int(input())
+    deq = deque()
+    li = stdin.readline().rstrip().split()
+    print(li)
+    deque.append(li[0])
