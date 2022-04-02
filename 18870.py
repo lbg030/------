6c75a8299070a568c756c053c422481825c5046a
ByeongGwon <lee971009@naver.com>
2022-04-02 20:10:28 +0900

    Silver 1

--- 18870.py
+++ 18870.py
@@ -1,11 +1,12 @@
-from sys import stdin
-n = int(input())
-li = list(map(int, stdin.readline().rstrip().split()))
-print(li)
-minimum = min(li)
-idx = li.index(minimum)
+import sys
+N = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
+arr2 = []
 
-for x in li:
-    if x == li[idx]:
-        x = 0
-    li[idx] = 0
+arr2 = list(sorted(set(arr)))
+
+# print(arr2)
+dic = {arr2[i]: i for i in range(len(arr2))}
+
+for i in arr:
+    print(dic[i], end=' ')
