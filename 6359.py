4d6b0ab478ad303e3ffc0ec03dbf7ef7e1ae7ec2
ByeongGwon <lee971009@naver.com>
2022-04-02 12:06:45 +0900

    Bronze 2

--- 6359.py
+++ 6359.py
@@ -2,5 +2,13 @@ n = int(input())
 
 for _ in range(n):
     m = int(input())
-    li = [[True] * m]
-    print(li)
+    li = [False] + [True] * m
+    for i in range(2, m+1):
+        for j in range(i, m+1, i):
+            # print(j)
+            # print(li)
+            if(li[j] == True):
+                li[j] = False
+            else:
+                li[j] = True
+    print(li.count(True))
