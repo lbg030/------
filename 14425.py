4db98ff3541557d753531e3e19c562b061d5f4ab
ByeongGwon <lee971009@naver.com>
2022-04-03 10:51:41 +0900

    Silver 3

---
+++ 14425.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+result_list = []
+compared_list = []
+for _ in range(n):
+    result_list.append(input())
+
+for _ in range(m):
+    compared_list.append(input())
+
+print(result_list, compared_list)
