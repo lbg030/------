aaba5a28e80ee80b81bbdb3ae7af7957ab2a2af9
ByeongGwon <lee971009@naver.com>
2022-04-06 17:45:48 +0900

    Silver 3

--- 9375.py
+++ 9375.py
@@ -9,7 +9,7 @@ for i in range(t):
             weardict[wear[1]].append(wear[0])
         else:
             weardict[wear[1]] = [wear[0]]
-    print(weardict)
+    # print(weardict)
     cnt = 1  # 각 종류마다 항목의 개수
 
     for k in weardict:
