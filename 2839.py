15c4ca85d799e6ff53237347bf85c7c05dab44bb
ByeongGwon <lee971009@naver.com>
2022-04-13 22:38:42 +0900

    Bronze 1

--- 2839.py
+++ 2839.py
@@ -1,12 +1,18 @@
-sugar = int(input())
-
-bag = 0
-while sugar >= 0:
-    if sugar % 5 == 0:  # 5의 배수이면
-        bag += (sugar // 5)  # 5로 나눈 몫을 구해야 정수가 됨
-        print(bag)
+n = int(input())
+cnt = 0
+check = 0
+while n >= 0:
+    if n % 5 == 0:
+        cnt += n // 5
         break
-    sugar -= 3
-    bag += 1  # 5의 배수가 될 때까지 설탕-3, 봉지+1
-else:
+
+    n -= 3
+    cnt += 1
+    if(0 < n < 3):
+        check = 1
+        break
+
+if(check == 1):
     print(-1)
+else:
+    print(cnt)
