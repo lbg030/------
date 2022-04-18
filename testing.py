da63c0013d4d372fb0b965469169131a84d38409
ByeongGwon <lee971009@naver.com>
2022-04-18 18:23:07 +0900

    Silver 5

--- testing.py
+++ testing.py
@@ -1,9 +1,2 @@
-sorting = {}
-n = input()
-li = list(map(int, input().split()))
-
-for i, x in enumerate(sorted(li)):
-    # sorting[sorted(li)[i]] = x
-    print(i, x)
-
-print(sorting)
+a = []
+print(a[-1])
