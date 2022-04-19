66600dfb577e3ce2815a8ac690aea5cfaad42786
ByeongGwon <lee971009@naver.com>
2022-04-19 21:19:04 +0900

    Silver 3

---
+++ 17413.py
@@ -0,0 +1,10 @@
+stack = list(input().split())
+checked = 0
+print(stack)
+# for i in range(len(stack)):
+#     if(stack[i] == '<'):
+#         checked = 1
+
+#     if(checked == 1):
+#         print(stack[i])
+#     else :
