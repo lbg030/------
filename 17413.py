6d06699ff5c73af7fb06453e88d5536cae145aac
ByeongGwon <lee971009@naver.com>
2022-04-19 21:30:09 +0900

    Silver 3

--- 17413.py
+++ 17413.py
@@ -1,12 +1,26 @@
 stack = list(input().split())
+tempList = []
 checked = 0
+ans = []
+
 print(stack)
-for i in range(len(stack)):
-    if(stack[i] == '<'):
-        checked = 1
+# # print(stack)
+# for k in range(len(stack)):
+#     for i in range(len(stack[k])):
+#         if(stack[k][i] == '<'):
+#             checked = 1
+#             ans.extend(tempList[::-1])
 
-    elif(stack[i] == '>'):
-        checked = 0
+#         elif(stack[k][i] == '>'):
+#             checked = 0
+#             tempList = []
 
-    if(checked == 1):
-        print(stack[i])
+#         if(checked == 1):
+#             ans.append(stack[k][i])
+#             # print(stack[k][i])
+
+#         elif(checked == 0):
+#             tempList.append(stack[k][i])
+#     print(f"ans = {ans}")
+#     print(f"tempList = {tempList}")
+# print(''.join(ans))
