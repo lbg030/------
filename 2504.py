5c376b5756837026bc3cb410c10baf366f181dc6
ByeongGwon <lee971009@naver.com>
2022-04-21 11:00:44 +0900

    Silver 2

--- 2504.py
+++ 2504.py
@@ -1,4 +1,4 @@
-lst = input()
+lst = input().rstrip()
 # 2에 대한 연산 스택
 stackTwo = []
 # 3에 대한 연산 스택
@@ -8,41 +8,37 @@ checked = True
 result = 0
 
 for i in range(len(lst)):
-    if lst[i] == ')':
+    if lst[i] == '(':
+        stackTwo.append(i)
+        value *= 2
+    elif lst[i] == '[':
+        stackThree.append(i)
+        value *= 3
+
+    elif lst[i] == ')':
         if(stackTwo):
-            if(lst[-1] == '('):
+            if(lst[i-1] == '('):
                 result += value
-            else:
-                checked = False
-                break
             stackTwo.pop()
-            value = value // 2
+            value //= 2
         else:
             checked = False
             break
     elif lst[i] == ']':
         if(stackThree):
-            if(lst[-1] == '['):
+            if(lst[i-1] == '['):
                 result += value
-            else:
-                checked = False
-                break
             stackThree.pop()
-            result = result // 3
+            value //= 3
 
         else:
+            # print(i)
+            # print(stackThree)
             checked = False
             break
 
-    elif lst[i] == '(':
-        stackTwo.append(i)
-        value *= 2
-    elif lst[i] == ']':
-        stackThree.append(i)
-        value *= 3
 
-if(checked == True):
-    if(not stackTwo and not stackThree):
-        print(result)
+if not stackTwo and not stackThree and checked:
+    print(result)
 else:
     print(0)
