1b444d8de08599434a674312d33138af70997864
ByeongGwon <lee971009@naver.com>
2022-04-21 10:43:18 +0900

    Silver 2

--- 2504.py
+++ 2504.py
@@ -1,22 +1,48 @@
-# 2나 3을 더한다는 개념이 아닌 무조건 2 or 3을 곱한다는 개념.
-
-def solve(lst):
-    for i in range(len(lst)):
-        if(lst[i] == ')'):
-            if(stack[-1] != '('):
-                return 0
-            else:
-                if(len(stack) > 1):
-                    tempList.append(2)
-                    pass
-
-        elif(lst[i] == ']'):
-            if(stack[-1] != '['):
-                return 0
-            else:
-
-
-lst = list(input())
-stack = []
-tempList = []
+lst = input()
+# 2에 대한 연산 스택
+stackTwo = []
+# 3에 대한 연산 스택
+stackThree = []
 value = 1
+checked = True
+result = 0
+
+for i in range(len(lst)):
+    if lst[i] == ')':
+        if(stackTwo):
+            if(lst[-1] == '('):
+                result += value
+            else:
+                checked = False
+                break
+            stackTwo.pop()
+            value = value // 2
+        else:
+            checked = False
+            break
+    elif lst[i] == ']':
+        if(stackThree):
+            if(lst[-1] == '['):
+                result += value
+            else:
+                checked = False
+                break
+            stackThree.pop()
+            result = result // 3
+
+        else:
+            checked = False
+            break
+
+    elif lst[i] == '(':
+        stackTwo.append(i)
+        value *= 2
+    elif lst[i] == ']':
+        stackThree.append(i)
+        value *= 3
+
+if(checked == True):
+    if(not stackTwo and not stackThree):
+        print(result)
+else:
+    print(0)
