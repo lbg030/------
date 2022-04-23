0a64a36eae370936381aae867df8abfd003c1136
ByeongGwon <lee971009@naver.com>
2022-04-23 13:44:12 +0900

    Gold 4

--- 16120.py
+++ 16120.py
@@ -1,2 +1,21 @@
-# P는 PPAP 문자열이다.
-# PPAP 문자열에서 P 하나를 PPAP로 바꾼 문자열은 PPAP 문자열이다.
+# p -> ppap로 바뀐것이기 때문에 결국 ppap -> p로 다시 바꾸어 준다면
+# 이 문자열이 ppap로 구성되어 있는지 아닌지 확인 가능하다.
+
+# pppapap -> pPap [ P = ppap ]
+# ppapapp -> Papp [ P = ppap ] 이런식으로
+
+w = input()
+stack = []
+ppap = ["P", "P", "A", "P"]
+for i in range(len(w)):
+    stack.append(w[i])
+    # print(stack)
+    if stack[-4:] == ppap:
+        for _ in range(4):
+            stack.pop()
+        stack.append("P")
+
+if stack == ppap or stack == ["P"]:
+    print("PPAP")
+else:
+    print("NP")
