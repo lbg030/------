4d2b3a52713ae3d26da44fdcf71e3ed940313de7
ByeongGwon <lee971009@naver.com>
2022-04-25 15:33:16 +0900

    Silver 2

--- 8911.py
+++ 8911.py
@@ -1,1 +1,13 @@
 # (x축 max - min) * y축(max - min) 하면 됨
+import turtle
+
+
+n = int(input())
+
+for _ in range(n):
+    lst = input()
+    x, y = [], []
+    turtleFaced = ['front', 'left', 'right ', 'behind']
+    direction = turtleFaced[0]
+    for x in lst:
+        if x == 'F':
