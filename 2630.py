073d5bde00532ce84898f8cfa88c59a69c23239a
ByeongGwon <lee971009@naver.com>
2022-04-27 19:44:13 +0900

    Silver 3

--- 2630.py
+++ 2630.py
@@ -1,23 +1,41 @@
+# 정사각형으로 4분할 한다고 해서 쿼드트리라고 불린다고 한다 ..
+
 size = int(input())
 
 # board
 board = [list(map(int, input().split())) for _ in range(size)]
 
+# white = 0 , blue = 1
+result = []
 
-#white, blue
-white, blue = 0, 0
-check = []
-# print(board)
-for i in range(size):
-    check.extend(set(board[i]))
 
-if (len(set(check)) > 1):
-    pass
-else:
-    if check[0] == 0:
-        white += 1
+def solve(x, y, size):
+    color = board[x][y]
+
+    # X와 Y축 조사
+    for i in range(x, x+size):
+        for j in range(y, y+size):
+            if color != board[i][j]:
+                solve(x, y, size//2)
+                print(
+                    f"x ={x} y = {y}, i = {i}, j = {j}, size = {size}, color = {color}, result = {result}")
+                solve(x, y + size//2, size//2)
+                print(
+                    f"x ={x} y = {y}, i = {i}, j = {j}, size = {size}, color = {color}, result = {result}")
+                solve(x + size//2, y, size//2)
+                print(
+                    f"x ={x} y = {y}, i = {i}, j = {j}, size = {size}, color = {color}, result = {result}")
+                solve(x + size//2, y+size//2, size//2)
+                print(
+                    f"x ={x} y = {y}, i = {i}, j = {j}, size = {size}, color = {color}, result = {result}")
+                return
+    if color == 0:
+        result.append(0)
     else:
-        blue += 1
+        result.append(1)
 
-# print(white)
-# print(blue)
+
+solve(0, 0, size)
+print(result.count(0))
+print(result.count(1))
+# print(result)
