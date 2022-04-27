b7385e7892cefb476331b0f0053f44e1e75f2d6f
ByeongGwon <lee971009@naver.com>
2022-04-28 08:55:24 +0900

    Silver 3

--- 11659.py
+++ 11659.py
@@ -1,7 +1,8 @@
 # start, finish 라인 쪽 input().split()으로 하면 시간 초과 남.
+# stdin.readline()으로 해도 마찬가지
 
 from sys import stdin
-
+from collections import deque
 number, case = map(int, input().split())
 
 lst = list(map(int, input().split()))
