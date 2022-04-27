ff731386bfeeaf95e865067f74dd7887fd43db7f
ByeongGwon <lee971009@naver.com>
2022-04-27 19:22:49 +0900

    Silver 3

--- 2630.py
+++ 2630.py
@@ -6,4 +6,18 @@ board = [list(map(int, input().split())) for _ in range(size)]
 
 #white, blue
 white, blue = 0, 0
-print(board)
+check = []
+# print(board)
+for i in range(size):
+    check.extend(set(board[i]))
+
+if (len(set(check)) > 1):
+    pass
+else:
+    if check[0] == 0:
+        white += 1
+    else:
+        blue += 1
+
+# print(white)
+# print(blue)
