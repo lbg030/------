1c73a22e363e6514ce554cb41b0ea4523772bd0d
ByeongGwon <lee971009@naver.com>
2022-04-30 11:49:45 +0900

    Silver 1

--- 11660.py
+++ 11660.py
@@ -1,9 +1,14 @@
 size, case = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(size)]
+for i in range(size):
 
 for i in range(size):
     for j in range(size):
-        pass
+        if(i == 0):
+            board[i][j] = sum(board[i][:j+1])
+        else:
+            if j == 0:
+                board[i][j] = board[i-1][size-1] + board[i][j]
 
 for _ in range(case):
     x1, y1, x2, y2 = map(int, input().split())
