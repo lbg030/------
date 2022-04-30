0a44c87b0cb7e1f9b4fded785a165b0ec37ca750
ByeongGwon <lee971009@naver.com>
2022-04-30 13:15:49 +0900

    Silver 1

--- 11660.py
+++ 11660.py
@@ -2,28 +2,28 @@
 size, case = map(int, input().split())
 
 # 보드판
-board = [list(map(int, input().split())) for _ in range(size)]
+board = [[0] + list(map(int, input().split())) for _ in range(size)]
 
 for i in range(size):
-    for j in range(size):
-
-        # 첫번째
-        if(i == 0 and j == 0):
+    for j in range(size+1):
+        if i == 0 and j == 0:
             continue
+        elif i != 0 and j == 0:
+            board[i][j] = board[i-1][-1]
 
         else:
-            if j == 0:
-                board[i][j] = board[i-1][size-1] + board[i][j]
-            else:
-                board[i][j] = board[i][j-1] + board[i][j]
+            board[i][j] = board[i][j-1] + board[i][j]
 
-# 테스트 케이스 입력받고 출력
 for _ in range(case):
     x1, y1, x2, y2 = map(int, input().split())
-    result = 0
-    if(x1 == 0 and y1 == 0):
-        print(board[x2-1][y2-1])
+    second = board[x2-1][y2]
+    first = board[x1-1][y1-1]
+    # print(second, first)
+    if x1 == 1 and y1 == 1:
+        result = second
+    elif x1 == x2 and y1 == y2:
+        result = second - board[x1-1][y1-2]
     else:
-        print(board[x2-1][y2] - board[x1-1][y1-1])
-
-# print(board)
+        result = second - first
+    print(result)
+print(board)
