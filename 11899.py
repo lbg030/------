84e4eb9d16924c63b71b1404f0467b4825926c48
ByeongGwon <lee971009@naver.com>
2022-05-02 20:51:51 +0900

    Silver 3

---
+++ 11899.py
@@ -0,0 +1,12 @@
+n = input()
+stack = []
+error = 0
+for i in range(len(n)):
+    if n[i] == '(':
+        stack.append(n[i])
+
+    else:
+        if stack[-1] == '(':
+            stack.pop()
+        else:
+            error += 1
