0b582f6bda020900b87a4616dbc3ba419af9e221
ByeongGwon <lee971009@naver.com>
2022-05-03 11:38:31 +0900

    Gold 4

--- 7662.py
+++ 7662.py
@@ -1,39 +1,68 @@
 # heap과 우선순위 큐는 Mini구하기는 쉽지만 max 하는건 어렵다
 # 우선순위 큐 두개를 사용해서 하나는 정상 하나는 -를 붙여서 최대 최소를 구한다.
-
+# 동일 수 3개를 넣었을 때 heapify가 정상적으로 되지 않음
+import sys
 import heapq
-testCase = int(input())
+case = int(input())
+for _ in range(case):
+    cases = int(input())
+    # 최소 최대 리스트 생성
+    maxList = []
+    minList = []
 
-for _ in range(testCase):
-    lst = []
-    case = int(input())
+    # 삭제가 되었는지 안되었는지 확인
+    removed = [0]*cases
+    for i in range(cases):
+        a, b = sys.stdin.readline().split()
 
-    # 케이스 개수 만큼 반복
-    for _ in range(case):
-      # n = I or D / m = 정수
-        n, m = input().split()
-        m = int(m)
+        if a == 'I':
+          # 최대 힙에다가는 -값 넣어서 정렬되게 하고 최소힙에는 그대로 값 넣는다
+          # i는 나중에 삭제되었는지 안되었는지 확인하기 위해 같이 넣는다.
+            heapq.heappush(maxList, ((-1)*int(b), i))
+            heapq.heappush(minList, (int(b), i))
 
-        # insert
-        if n == 'I':
-            heapq.heappush(lst, m)
+        # 삭제
+        # -1일때는 최소 값 / 1일 때는 최대 값
+        elif a == 'D':
 
-        # delete
+            # 최솟값
+            if b == '-1':
+                while minList:
+                  # 만약 이미 제거된 문자열이 앞 존재한다면 전부 다 제거
+                    if removed[minList[0][1]] == 1:
+                        heapq.heappop(minList)
+                    else:
+                        break
+                if minList:
+                    # min은 i의 값을 가지고 와서 Remove[i]값을
+                    # 이미 제거 되었다는 뜻으로 1로 변환 시킴
+                    min = minList[0][1]
+                    removed[min] = 1
+                    heapq.heappop(minList)
+
+            # 최대값
+            elif b == '1':
+                while maxList:
+                    if removed[maxList[0][1]] == 1:
+                        heapq.heappop(maxList)
+                    else:
+                        break
+                if maxList:
+                    max = maxList[0][1]
+                    removed[max] = 1
+                    heapq.heappop(maxList)
+
+    while maxList:
+        if removed[maxList[0][1]] == 1:
+            heapq.heappop(maxList)
         else:
-            # -1일때 최소 / 1일때 최대
-            if m == -1:
-                if lst:
-                    heapq.heappop(lst)
-            else:
-                if lst:
-                    max_lst = [-x for x in lst]
-                    heapq.heapify(max_lst)
-                    heapq.heappop(max_lst)
-                    lst = [-x for x in max_lst]
-                    heapq.heapify(lst)
-        print(lst, n, m)
-
-    if not lst:
-        print("Empty")
+            break
+    while minList:
+        if removed[minList[0][1]] == 1:
+            heapq.heappop(minList)
+        else:
+            break
+    if minList:
+        print((-1)*maxList[0][0], minList[0][0])
     else:
-        print(lst[-1], lst[0])
+        print("EMPTY")
--- testing.py
+++ testing.py
@@ -1,16 +1,12 @@
-# import heapq
-# h = [(3, "Go to home"), (10, "Do not study"),
-#      (1, "Enjoy!"), (4, "Eat!"), (7, "Pray!")]
-# max_h = [(-ele[0], ele[1]) for ele in h]
-# heapq.heapify(max_h)
-# print(max_h)
 
-# heapq.heappop(max_h)
+from queue import PriorityQueue
 
-# h = [(-ele[0], ele[1]) for ele in max_h]
-# heapq.heapify(h)
-# print(h)
 
-a = [1]
+a = PriorityQueue()
 
-print(a[0], a[-1])
+a.put(3)
+a.put(2)
+a.put(6)
+
+for x in a:
+    print(x)
