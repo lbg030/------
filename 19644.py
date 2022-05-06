f96bcbd0b2e343439836f998f4a4c80d0e0c06b7
ByeongGwon <lee971009@naver.com>
2022-05-06 22:00:42 +0900

    Gold 3

--- 19644.py
+++ 19644.py
@@ -6,6 +6,7 @@ mine = int(input())
 zombie = deque([])
 # 마인이 몇개 필요한지
 cnt = 0
+check = 0
 for i in range(roadLength):
     zombie.append([i+1, int(stdin.readline().rstrip())])
 
@@ -13,15 +14,20 @@ for i in range(roadLength):
 # 빡구현말고 논리적으로 풀어보기
 for i, x in zombie:
     if i < rifleLength:
-        if damage * (i - cnt) < x:
+        if damage * (i - check) < x:
             cnt += 1
-        # print(i, damage * (i - cnt))
+            check += 1
+        # print(i, damage * (i - check))
     else:
-        if (rifleLength - cnt) * damage < x:
+        if (rifleLength - check) * damage < x:
             cnt += 1
-        # print(i, (rifleLength - cnt) * damage)
-    # print(i, (rifleLength - cnt) * damage)
+            check += 1
+        # print(i, (rifleLength - check) * damage)
+    # print(i, (rifleLength - check) * damage)
     # print(i, x, cnt)
+    if check > 0:
+        if x == 0:
+            check -= 1
 if cnt > mine:
     print('NO')
 else:
