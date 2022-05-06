52c5c15cdb3952182a5225f4c6867e6114f5fa1a
ByeongGwon <lee971009@naver.com>
2022-05-06 21:33:57 +0900

    1

--- 19644.py
+++ 19644.py
@@ -4,9 +4,18 @@ roadLength = int(input())
 rifleLength, damage = map(int, input().split())
 mine = int(input())
 zombie = deque([])
+# 마인이 몇개 필요한지
+cnt = 0
 
 for i in range(roadLength):
     zombie.append([i+1, int(stdin.readline().rstrip())])
 
 print(f"원래 좀비 = {zombie}")
 # 빡구현말고 논리적으로 풀어보기
+for i, x in zombie:
+    if i < rifleLength:
+        if damage * i < x:
+            cnt += 1
+    else:
+        if rifleLength * damage < x:
+            cnt += 1
