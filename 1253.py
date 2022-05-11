5f01e5397ea087e1bd7060c8dceea3111d2d5c06
ByeongGwon <lee971009@naver.com>
2022-05-11 12:24:36 +0900

    Gold 4

--- 1253.py
+++ 1253.py
@@ -1,5 +1,7 @@
 # 1. 모든 경우의 경우의 수로 도전
-# 2. 
+# -> 두 값을 더하면 그 두값을 뺀 리스트를 만들어 그 안에 있는지 비교
+# -> 처음부터 모든 경우의 수를 더하지 말고 하나씩 비교.
+# -> 만약 되었다면 그 값은 비교 리스트에서 아웃.
 from sys import stdin
 from itertools import combinations
 
@@ -8,24 +10,9 @@ n = int(input())
 #숫자들 리스트
 lst = sorted(list(map(int, stdin.readline().rstrip().split())))
 
-possibleList = set()
-#경우의 수 리스트
-possible = list(combinations(lst, 2))
-for x in possible:
-    possibleList.add(sum(x))
 
-possibleList = list(possibleList)
 
-#범위 내에 있는 값만 추려논 possibleList 만들기 
-while True:
-    for i in range(len(possibleList)-1, 0 , -1):
-        if possibleList[i] > lst[-1] :
-            possibleList.pop()
-        else :
-            break
-    break
 
-print(possibleList)
 #이분 탐색 알고리즘
 def binary_search(target, data):
     start = 0
@@ -42,10 +29,3 @@ def binary_search(target, data):
             end = mid -1
 
     return 0
-
-
-cnt = 0
-for x in lst:
-    cnt += binary_search(x, possibleList)
-
-print(cnt)
