d039d132e3611bb15abe7413d430d315e4fba962
ByeongGwon <lee971009@naver.com>
2022-05-13 21:45:34 +0900

    1

--- testing.py
+++ testing.py
@@ -1,6 +1,15 @@
-dic = {'1': [0], '2': [20]}
+genres = ["classic", "pop", "classic", "classic", "pop"]
+plays = [500, 600, 150, 800, 2500]	
 
-dic['1'].append(20)
+dic = {}
+
+for i in range(len(genres)):
+        genresDic = genres[i] 
+        if genres[i] not in dic :    
+            dic[genresDic] = [0]
+            dic[genresDic].append(plays[i])
+        else :
+            dic[genresDic].append(plays[i])
 
 print(dic.keys(), dic.values())
 
