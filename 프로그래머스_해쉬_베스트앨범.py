5c8ef5834d223232332b27e3cbd42482c7550808
ByeongGwon <lee971009@naver.com>
2022-05-13 20:30:15 +0900

    해쉬

---
+++ 프로그래머스_해쉬_베스트앨범.py
@@ -0,0 +1,7 @@
+genres = ["classic", "pop", "classic", "classic", "pop"]
+plays = [500, 600, 150, 800, 2500]	
+#output = [4, 1, 3, 0]
+
+def solution(genres, plays):
+    answer = []
+    return answer
