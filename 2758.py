af03899bd858ea4613662d92452fffaad49c3297
ByeongGwon <lee971009@naver.com>
2022-05-13 17:46:18 +0900

    Gold 4

--- 2758.py
+++ 2758.py
@@ -1,13 +1,12 @@
-#로또
-#이게 왜 DP문제지
-# 1 2 4 8
-# 1 2 4 9
-# 1 2 4 10
-# 1 2 5 10
+dp = [[0]*2001 for i in range(11)]
+dp[0] = [1]*2001
+for i in range(1, 11):
+    for j in range(1, 2001):
+        dp[i][j] = dp[i][j-1]+dp[i-1][j//2]
 
-test = int(input())
-for _ in range(test):
+T = int(input())
+for _ in range(T):
     n, m = map(int, input().split())
+    print(dp[n][m])
 
-dp = [i for i in range(1, m+1)]
-print(dp)
+print(dp[4][:11])
