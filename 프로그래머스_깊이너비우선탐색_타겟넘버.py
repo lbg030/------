536fc9efdfa114ce753e7dce5b52782b250169cf
ByeongGwon <lee971009@naver.com>
2022-05-19 10:35:03 +0900

    1

--- 프로그래머스_깊이너비우선탐색_타겟넘버.py
+++ 프로그래머스_깊이너비우선탐색_타겟넘버.py
@@ -3,7 +3,10 @@
 # [4, 1, 2, 1]	4	2
 
 #다 더한다음에 숫자를 빼주기
+#number = [1,1,1,1,1] target = 3
 def solution(numbers, target):
     answer = 0
+    
+    summation = sum(numbers)
     
     return answer
