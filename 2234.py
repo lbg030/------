3f7356fbc42ba602e3f10f64c711590fc1ac2daf
ByeongGwon <lee971009@naver.com>
2022-05-19 14:33:12 +0900

    성곽

--- 2234.py
+++ 2234.py
@@ -3,6 +3,12 @@
 # 동쪽에 벽이 있을 때는 4를, 
 # 남쪽에 벽이 있을 때는 8을 더한 값이 주어진다.
 
+#이 성에 있는 방의 개수
+# 가장 넓은 방의 넓이
+# 하나의 벽을 제거하여 얻을 수 있는 가장 넓은 방의 크기
+def bitExpression(k):
+    return bin(int(k))[2:].zfill(4)
+
 N,M = map(int, input().split())
-lst = [list(map(int, input().split())) for _ in range(M)]
+lst = [list(map(bitExpression, input().split())) for _ in range(M)]
 print(lst)
