0f63dbab1276b0e27539f207c2a1470660bf4df1
ByeongGwon <lee971009@naver.com>
2022-05-20 08:35:03 +0900

    1

--- 2234.py
+++ 2234.py
@@ -8,7 +8,8 @@
 # 하나의 벽을 제거하여 얻을 수 있는 가장 넓은 방의 크기
 def bitExpression(k):
     return bin(int(k))[2:].zfill(4)
-
+#이진수로 봤을때 이동은 남동북서 순서임
 N,M = map(int, input().split())
 lst = [list(map(bitExpression, input().split())) for _ in range(M)]
-print(lst)
+# print(lst)
+visit = [[0] * N for _ in range(M)]
