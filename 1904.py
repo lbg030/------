8d7e8235ba66508762f32be3557373e90f9d13ca
ByeongGwon <lee971009@naver.com>
2022-05-24 21:57:45 +0900

    Silver 3

--- 1904.py
+++ 1904.py
@@ -1,16 +1,11 @@
 
-n =int(input())
+n = int(input())
 # 1 2 3 5 7 12
-dp = [0,1,2] # dp[i-2] * 2 +1
-if n == 1:
-    print(dp[0])
-elif n == 2:
-    print(dp[1])
-else :
-    for i in range(3,n+1):
-        a = dp[i-2] + dp[i-1]
-        if a >= 15746 :
-            a %= 15746
-        dp.append(a)
-    print(dp[n])
-    # print(dp)
+dp = [0] * (n+2)
+dp[1] = 1
+dp[2] = 2
+for i in range(3, n+1):
+    a = (dp[i-2] + dp[i-1]) % 15746
+    dp[i] = a
+print(dp[n])
+# print(dp)
