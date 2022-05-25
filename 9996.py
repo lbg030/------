459a8c48007a27ef1b212b4dadb0268c83fea916
ByeongGwon <lee971009@naver.com>
2022-05-25 14:31:32 +0900

    Silver 3

--- 9996.py
+++ 9996.py
@@ -1,9 +1,22 @@
+
 Case = int(input())
 target = list(map(str, input().split('*')))
-
-for i in range(Case):
-    #무조건 2개로 나뉘게 되어있음.
-    first = len(target[0])
-    last = len(target[1])
+first = len(target[0])
+last = len(target[1])
+#별표는 문자열의 시작과 끝에 있지 않음 -> 무조건 2개로 나눠짐.
+for i in range(Case):    
     compared = input()
+    
+    if target[0] == compared[:first]:
+        compared = compared[first:]
+        if len(compared) == 0 :
+            print("NE")
+            continue
+        # print(compared)
+        if target[1] == compared[-last:]:
+            print("DA")
+        else :
+            print("NE")
+    else :
+        print("NE")
     
