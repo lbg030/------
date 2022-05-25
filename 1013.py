883cfcbfb1e2e407a3834dbf8eac4aaec527e889
ByeongGwon <lee971009@naver.com>
2022-05-25 20:13:46 +0900

    Gold 5

--- 1013.py
+++ 1013.py
@@ -1,54 +1,9 @@
-case = int(input())
-#(100+1+ | 01)+
-lst = ['101','01']
-
-def overlap(a):
-    string = ''
-    for x in a:
-        if string:
-            if string[-1] != x:
-                string += x
-        else :
-            string += x
-    
-    return string
-
-for i in range(case):
-    compared = input()
-    queue = ''
-    queue2 = ''
-    # set해서 101 or 01이 되면 
-    for x in compared:
-        if not queue:
-            queue += x
-        
-        else :
-            if queue == '01':
-                queue =''
-                queue += x
-            elif queue == '100':
-               if x == '1':
-                    queue += x
-            elif queue == '1001':
-                if x == '0':
-                    queue = '0'
-            else:
-                queue += x
-    # print(queue)
-    if queue in lst:
+import re
+t = int(input())
+for i in range(t):
+    a = input()
+    p = re.compile('(100+1+|01)+')
+    if p.fullmatch(a):
         print("YES")
-    else :
-        if queue:
-            # print(len(queue), queue[:3])
-            if len(queue) < 4 or queue[:3] == 100:
-                # print("??")
-                print("NO")
-            else:
-                a = overlap(queue)
-                # print(a)
-                if a in lst:
-                    print("YES")
-                else:
-                    print("NO")
-        else:
-            print("YES")
+    else:
+        print("NO")
