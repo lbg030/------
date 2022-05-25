e295114ed66d0552ce4ec8af06c0749ac23f1dea
ByeongGwon <lee971009@naver.com>
2022-05-25 12:13:57 +0900

    1

--- 1012.py
+++ 1012.py
@@ -4,21 +4,12 @@ for i in range(testCase):
     
     #한칸 더 추가한 이유는 오른쪽과 아래쪽을 비교할 건데 
     # 그래프의 끝에선 인덱스 에러가 나는 것을 방지하기 위해
-    graph = [[0] * (width+1) for _ in range(length+1)] # 가로 세로만큼 그래프
+    graph = [[0] * (width) for _ in range(length)] # 가로 세로만큼 그래프
     cnt = 0
     #배추 위치 심기
     for i in range(cabbage):
         x, y = map(int , input().split())
         graph[y][x] = 1
 
-    #그래프 탐색
-    for i in range(width):
-        for j in range(length):
-            if graph[j][i] == 1:
-                if graph[j+1][i] == 0 and graph[j][i+1] == 0:
-                    print(j, i)
-                    cnt += 1
     print(cnt)
-    print(graph)
-    
-    #들어온 Input에 대해서만 그래프 탐색을 돌리면 쓸모없는거 할 필요 없음 .
+    print(graph)    
