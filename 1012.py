75bdf71f12b0099fd983e7bb90c767ee6c38e535
ByeongGwon <lee971009@naver.com>
2022-05-25 12:34:27 +0900

    Silver 2

--- 1012.py
+++ 1012.py
@@ -1,4 +1,28 @@
+from collections import deque
+
 testCase = int(input())
+
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+
+def bfs(x,y, graph):
+    queue = deque()
+    queue.append((x,y))
+    graph[x][y] = 0
+    
+    while queue :
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if nx < 0 or ny < 0 or nx >= length or ny >= width :
+                continue
+            
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                queue.append((nx,ny))
+    return 
 for i in range(testCase):
     width, length, cabbage = map(int, input().split()) #가로, 세로, 배추위치
     
@@ -8,8 +32,13 @@ for i in range(testCase):
     cnt = 0
     #배추 위치 심기
     for i in range(cabbage):
-        x, y = map(int , input().split())
-        graph[y][x] = 1
+        y, x = map(int , input().split())
+        graph[x][y] = 1
 
+    for x in range(length):
+        for y in range(width):
+            if graph[x][y] == 1:
+                bfs(x,y, graph)
+                cnt += 1
     print(cnt)
-    print(graph)    
+    print(graph)
