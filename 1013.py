16e120e22b8a292f75cb51732eae168f0a7e1007
ByeongGwon <lee971009@naver.com>
2022-05-25 18:54:55 +0900

    1

--- 1013.py
+++ 1013.py
@@ -1,11 +1,27 @@
-from collections import deque
-
 case = int(input())
-
 #(100+1+ | 01)+
-
-lst = ['100','1','01']
+lst = [['1','0','1'], ['0','1']]
 for i in range(case):
     compared = input()
-    while compared :
+    queue = []
+    
+    # set해서 101 or 01이 되면 
+    queue.append(compared[0])
+    for x in compared:
+        if queue :
+            if x != queue[-1] :
+                queue.append(x)
+        else :
+            queue.append(x)
+            
         
+        if queue[:2] in lst :
+            queue = queue[2:]
+        elif queue[:3] in lst:
+            queue = queue[3:]
+            
+        print(queue)
+    if queue :
+        print("NO")
+    else :
+        print("YES")
