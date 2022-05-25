70c1d17de9b6ef8319e26a131efc97d0420e3c06
ByeongGwon <lee971009@naver.com>
2022-05-25 18:57:50 +0900

    1

--- 1013.py
+++ 1013.py
@@ -6,21 +6,9 @@ for i in range(case):
     queue = []
     
     # set해서 101 or 01이 되면 
-    queue.append(compared[0])
     for x in compared:
-        if queue :
-            if x != queue[-1] :
-                queue.append(x)
-        else :
-            queue.append(x)
-            
         
-        if queue[:2] in lst :
-            queue = queue[2:]
-        elif queue[:3] in lst:
-            queue = queue[3:]
-            
-        print(queue)
+
     if queue :
         print("NO")
     else :
