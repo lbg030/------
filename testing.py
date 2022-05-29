9edbb66c490e814f5409e2f1d36a09321ec604ca
lbg030 <lee971009@naver.com>
2022-05-30 07:50:37 +0900

    1

--- testing.py
+++ testing.py
@@ -1,5 +1,7 @@
-def test(a):
-    return "입력값:%s"%a
+a = int(input("1보다 큰 양의 정수를 입력하세요. "))
 
+answer = 1
+for i in range(1,a+1):
+    answer = answer * i
 
-test(2)
+print("factorial data :", answer)
