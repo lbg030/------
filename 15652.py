978ccc7c744ccad778cb0c71c54b8dd7a0030209
ByeongGwon <lee971009@naver.com>
2022-05-30 10:22:57 +0900

    Silver 3

--- 15652.py
+++ 15652.py
@@ -1,5 +1,10 @@
-n, m = map(int, input().split())
+from itertools import combinations_with_replacement
 
-li = [[x for x in range(1, n+1)]] * m
+n,m = map(int , input().split())
 
-print(li)
+li = [i for i in range(1,n+1)]
+
+lst = combinations_with_replacement(li, m)
+
+for x in lst :
+    print(x)
