2d019d196995f5f3c664035d7db576c96c5fd40c
ByeongGwon <lee971009@naver.com>
2022-06-01 21:52:22 +0900

    1

--- 11053.py
+++ 11053.py
@@ -6,6 +6,7 @@ for i in range(n):
     for j in range(i):
         if(li[i] > li[j]):
             # print(f"dp = {li[j]}, i = {i} j = {dp[j]}")
+            # print(dp)
             dp[i] = max(dp[i], dp[j]+1)
             # print(dp[i])
 
