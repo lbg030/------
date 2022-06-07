ddce9ccd6fe4a04ec5a07d0f222e112766d4cd21
ByeongGwon <lee971009@naver.com>
2022-06-07 19:59:25 +0900

    testing

--- testing.py
+++ testing.py
@@ -1,14 +1,46 @@
-## 1번
+player1 = []
+player2 = []
 
-for i in range(1, 101):
-    if i % 5 == 0 or i % 7 == 0:
-        continue
-    print(i)
+for _ in range(3):
+    player1.append(input())
+
+for _ in range(3):
+    player2.append(input())
     
-## 2번
+# print(player1, player2)
 
-num = 0
-while True:
-    if num >= 30 : break
-    print(num)
-    num += 1
+def game(shape, color, number):
+    
+    #처음에 숫자는 str
+    number = int(number)
+    result = 0
+    
+    if shape == 'HEART' :
+        #빨간색 하트
+        if color == 'RED':
+            result = number * 2
+        #초록색 하트
+        else :
+            result = number - 1
+    
+    if shape == 'CLOVER':
+        #빨간색 클로버
+        if color == 'RED':
+            result = 0
+        #초록색 클로버
+        else :
+            result = number + 2
+    
+    #결과값 출력
+    return result
+
+player1 = game(player1[0],player1[1],player1[2])
+player2 = game(player2[0],player2[1],player2[2])
+
+if player1 > player2 :
+    print("player1 win, player2 lose")
+elif player2 > player1 :
+    print("player1 lose, player2 win")
+else :
+    print("draw")
+    
