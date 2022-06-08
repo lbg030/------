0cb4c0881251b23dd403e9dcf8d89c7ecd50ef97
ByeongGwon <lee971009@naver.com>
2022-06-08 14:16:43 +0900

    Silver 1

--- 2667.py
+++ 2667.py
@@ -1,3 +1,4 @@
+from collections import deque
 n = int(input())
 
 graph = [ [0] * (n+1) for _ in range(n+1)]
@@ -10,29 +11,35 @@ for i in range(n):
 cnt = 0
 
 def bfs(x, y):
-        answer = []
+        answer = deque()
+        graph[x][y] = 0
         count = 0
-        answer.append(x,y)
+        answer.append([x,y])
         dx = [0,0,1,-1]
         dy = [1,-1,0,0]
         while answer:
+            # print(answer, count)
+            count += 1
+            x,y = answer.popleft()
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
                 
                 if graph[nx][ny] == 1:
                     graph[nx][ny] = 0
-                    answer.append(nx,ny)
-                    count += 1
+                    answer.append([nx,ny])
                     
-            return count
+        return count
+
 
 lstCnt = []
 for i in range(n):
     for j in range(n):
         if graph[i][j] == 1:
             cnt += 1
-            count = 0
-            bfs(i,j)
+            count = bfs(i,j)
             lstCnt.append(count)
-print(cnt, lstCnt)
+            
+print(cnt)
+for x in sorted(lstCnt):
+    print(x)
