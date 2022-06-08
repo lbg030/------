d1f1bfdf6c8bdce9aaa52c5a641fdb11f850e846
ByeongGwon <lee971009@naver.com>
2022-06-08 14:01:19 +0900

    테스팅

--- testing.py
+++ testing.py
@@ -1,15 +1,20 @@
-lst = list(map(str, input().split('`')))
-n = 1
-while True:
-    str= ''
-    for x in lst:
-        for y in x:
-            try :
-                str += chr(ord(y)-n)
-            except:
-                str += y
-    if "세명" in str:
-        break
+x, y = map(float, input("두 수를 입력하세요: ").split())
+
+print("1.더하기 2. 빼기 3. 곱하기 4. 나누기")
+method = int(input("계산 방식을 숫자로 선택하세요. "))
+
+
+if method == 1:
+    print(x+y)
+
+elif method == 2:
+    print(x - y)
     
-    n += 1
-print(str)
+elif method == 3:
+    print(x*y)
+    
+else :
+    if y == 0:
+        print('0으로 나눌 수 없습니다.')
+    else :
+        print(x/y)
