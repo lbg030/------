47a4117fdeb0ffcf5310e844563adea821a52173
ByeongGwon <lee971009@naver.com>
2022-06-08 18:38:14 +0900

    1

--- testing.py
+++ testing.py
@@ -1,10 +1,13 @@
-def f(x):
-    n = len(x)
-    for k in range(n-1) :
-        if x[k] > x[k+1]:
-            x[k], x[k+1] = x[k+1], x[k]
-            
-    return x
+a = input()
+b = ''
+for i in range(len(a)):
+    if not b :
+        b += a[i]
+        continue
+    
+    else :
+        if b[-1] == ' ' and a[i] == ' ':
+            continue
 
-
-print(f([40,30,10,20]))
+    b += a[i]
+print(b)
