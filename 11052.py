3856a793ab85e70278c6306de9d805faf5098eef
ByeongGwon <lee971009@naver.com>
2022-06-09 10:13:47 +0900

    Silver 1

--- 11052.py
+++ 11052.py
@@ -1,15 +1,4 @@
 n = int(input())
 lst = list(map(int, input().split()))
-dp = [0,lst[0]]
+dp = [0] * n
 
-if dp[1] * 2 > lst[1] :
-        dp.append(dp[1]*2)
-else :
-        dp.append(lst[1])
-        
-# dp = [0,1,5]
-for i in range(2,n):
-    
-    dp.append(max(lst[i], dp[i]+dp[1], dp[i-2] + dp[2]))
-
-print(dp)
