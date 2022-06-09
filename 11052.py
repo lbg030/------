7628d0f45363e3561c41435ff1ee2119fdfedeef
ByeongGwon <lee971009@naver.com>
2022-06-09 15:18:19 +0900

    Silver 1

--- 11052.py
+++ 11052.py
@@ -1,4 +1,11 @@
 n = int(input())
-lst = list(map(int, input().split()))
-dp = [0] * n
+lst = [0]+list(map(int, input().split()))
+dp = [0] * (n+1)
 
+dp[0] = lst[0]
+
+for i in range(1,n+1):
+    for k in range(1, i+1):
+        dp[i] = (max(dp[i], dp[i-k] + lst[k]))
+    
+print(dp)
