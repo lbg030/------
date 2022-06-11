cd7f81c8fe5dcc8176c0cd18c78293143cccadc3
ByeongGwon <lee971009@naver.com>
2022-06-12 00:48:15 +0900

    1

--- testing.py
+++ testing.py
@@ -1,5 +1,6 @@
-while True:
-    try:
-        print(input())
-    except:
-        break
+from sys import stdin
+lst = list(map(int, stdin.readline().rstrip().split(',')))
+sum = sum(lst)
+mean = round(sum/(len(lst)-1),1)
+
+print(f"합계 {sum} 평균 {mean} ")
