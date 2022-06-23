a0db8379d55851e252ee1cd1971365bfba5046e1
ByeongGwon <lee971009@naver.com>
2022-06-23 09:11:39 +0900

    Gold 5

--- 7576.py
+++ 7576.py
@@ -4,4 +4,15 @@ n,m = map(int, input().split())
 
 lst = [list(map(int, stdin.readline().rstrip().split())) for i in range(m)]
 
-print(lst)
+#안되는 경우 cnt로 판별
+cnt = 0
+
+templist = []
+
+for i in range(m):
+    for j in range(n):
+        if lst[i][j] == 1 :
+            templist.append((i,j))
+            
+
+print(templist)
