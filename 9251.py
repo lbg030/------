8db94d3cffbf95ab8d880270b50fbcc10266b8fa
ByeongGwon <lee971009@naver.com>
2022-06-24 20:46:05 +0900

    1

--- 9251.py
+++ 9251.py
@@ -1,4 +1,22 @@
 a = list(input())
 b = list(input())
+dp = list([0] * (len(a)-1))
 
-print(a,b)
+
+for x in a:
+    if x not in b:
+        a.remove(x)
+
+cnt = 0
+temp = b[:]
+for x in a :
+        if x in temp :
+            start = temp.index(x)
+            cnt += 1
+            if start + 1 > len(temp)-1 :
+                break
+            temp = temp[start+1:]
+        else :
+            continue
+
+print(cnt)
