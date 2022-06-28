8173598a63c304380e0acbc3ed0db32605477440
ByeongGwon <lee971009@naver.com>
2022-06-28 18:42:55 +0900

    Gold 5

--- 7569.py
+++ 7569.py
@@ -1,33 +1,50 @@
 from sys import stdin
 from collections import deque
-n, m, l = map(int, input().split())
+n, m, h = map(int, input().split())
 
-lst = [list(map(int, stdin.readline().rstrip().split())) for i in range(m*l)]
-
+lst = [list(map(int, stdin.readline().rstrip().split())) for i in range(m*h)]
+# print(lst)
 
 cnt = 0
 templist = deque()
 temp = []
 
+zeroCount = 0
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
-for i in range(m):
+for i in range(m*h):
     for j in range(n):
         if lst[i][j] == 1:
             temp.append((i, j))
+        else :
+            zeroCount += 1
     
 templist.append(temp)
 
-
-while templist:
+while templist and zeroCount != 0:
     temp = []
     cnt += 1
-    # 3 과 5 가 들어감
     k = templist.popleft()
     for i in k:
         x,y = i
+        # print(x,y)
         # print(f"x = {x}, y = {y}")
         # print(f"templist {templist}")
+        px = x + m
+        mx = x - m
+        
+        if 0 <= px < m*h:
+            if lst[px][y] == 0:
+                lst[px][y] = 1
+                temp.append((px,y))
+                zeroCount -= 1
+                
+        if 0 <= mx < m*h:
+            if lst[mx][y] == 0:
+                lst[mx][y] = 1
+                temp.append((mx,y))
+                zeroCount -= 1
+                
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
@@ -38,16 +55,19 @@ while templist:
             else:
                 if lst[nx][ny] == 0:
                     lst[nx][ny] = 1
+                    zeroCount -= 1
                     temp.append((nx, ny))
                 else : # 여기도 생각 다시 해봐야됨
                     continue
                 
-    #여기서 templist 요소 한번 사이클 끝
-    # print(x,y, cnt)
-    if temp:
+    
+    if len(temp) > 0:
         templist.append(temp)
-        
-    # print(templist)
+
+    print(lst, cnt, zeroCount, templist)
+    
+    if zeroCount == 0:
+        break
     
 flag = 1
 
@@ -58,4 +78,6 @@ for x in lst:
 if flag == 0 :
     print('-1')
 else :
-    print(cnt - 1)
+    print(cnt-1)
+
+# print(lst)
--- testing.py
+++ testing.py
@@ -1,10 +1,7 @@
 from collections import deque
 t = deque()
-lst = []
-lst.append((1,2))
-lst.append((2,3))
+print(t)
 
-t.append(lst)
-k = deque([[]])
-print(len(t))
-print(len(k))
+temp = []
+
+print(len(temp))
