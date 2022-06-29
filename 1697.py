dd7866052e2eea31441e672db2f3fd6801d2c31e
ByeongGwon <lee971009@naver.com>
2022-06-29 17:58:46 +0900

    Silver 1

--- 1697.py
+++ 1697.py
@@ -1,26 +1,24 @@
 from collections import deque
-n, m =map(int,input().split())
 
-lst  = deque([n])
-cnt = 0
+### n = 5 , m = 17
+n,m = map(int, input().split())
+maximum = 100001
+dp = [0] * maximum 
 
-if n == m :
-    print(cnt)
-else :
-    while True :
-        temp = []
-        for x in lst:
-            add = x + 1
-            minus = x - 1
-            teleport = x * 2
-            temp.append(add)
-            temp.append(minus)
-            temp.append(teleport)
-        cnt += 1
-        
-        if m in temp:
-            print(cnt)
+def bfs(n):
+    queue = deque()
+    queue.append(n)
+    while queue:
+        check = queue.popleft()
+        #종료 조건 check가 m이랑 같아 질 때까지 무한 반복
+        if check == m:
+            print(dp[m])
             break
-        else :
-            lst.extend(temp)
-            lst.popleft()
+        
+        for i in (check-1, check+1, check * 2):
+            if 0 <= i < maximum and dp[i] == 0 :
+                # i번째는 현재 check에서 한번만 수행하면 되기 때문에 +1로 처리
+                dp[i] = dp[check] + 1 
+                queue.append(i)
+
+bfs(n)
--- testing.py
+++ testing.py
@@ -1,62 +1,21 @@
+from collections import deque
 
-n, m = map(int, input().split())
-aList = []
-bList = []
+def bfs():
+    q=deque()
+    q.append(n)
+    while q:
+        now=q.popleft()
+        if now==k:
+            return
+        for i in (now-1,now+1,now*2):
+            print(i)
+            if 0<=i<100001 and dp[i]==0:
+                dp[i]=dp[now]+1
+                q.append(i)
 
-aDistance = 0
-bDistance = 0
-for _ in range(n):
-    b,a = input().split()
-    
-    b = int(b)
-    if a == "R":
-        for _ in range(b):
-            aDistance += 1
-            aList.append(aDistance)
-    else :
-        for _ in range(b):
-            aDistance -= 1
-            aList.append(aDistance)
-    
-for _ in range(m):
-    b,a = input().split()
-    b = int(b)
-    if a == "R":
-        for _ in range(b):
-            bDistance += 1
-            bList.append(bDistance)
-    else :
-        for _ in range(b):
-            bDistance -= 1
-            bList.append(bDistance)
+n,k=map(int,input().split())
+dp=[0]*100001
 
-if len(aList) < len(bList):
-    aList.extend(([aList[-1]] * (len(bList) - len(aList))))
-else :
-    bList.extend(([bList[-1]] * (len(aList) - len(bList))))
-    
-a,b = 0,0            
-cnt = 0
-maximum = max(len(aList), len(bList))
-
-if aList[0] == bList[0] :
-    cnt += 1
-
-# second = 2
-if maximum > 1:
-    for i in range(1, maximum):
-        # second += 1
-        
-        aTemp = aList[i-1]
-        a = aList[i]
-        bTemp = bList[i-1]
-        b = bList[i]
-
-        if a == b and (aTemp != bTemp):
-            cnt += 1
-            
-        # print(second)
-
-print(cnt)
-# print(aList)
-# print(bList)
+bfs()
+print(dp[k])
+print(dp[:k+1])
