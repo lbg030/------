2433c5866b77793f2a16248568b93a3f579cbdd0
ByeongGwon <lee971009@naver.com>
2022-06-30 23:43:52 +0900

    Gold 5

--- 12865.py
+++ 12865.py
@@ -1,22 +1,10 @@
-import sys
-
-N, K = map(int, input().split())
-stuff = [[0,0]]
-knapsack = [[0 for _ in range(K + 1)] for _ in range(N + 1)]
-
-for _ in range(N):
-    stuff.append(list(map(int, input().split())))
+number, bagWeight = map(int, input().split())
 
 
-#냅색 문제 풀이 N = 4 , K = 7
-for i in range(1, N + 1):
-    for j in range(1, K + 1):
-        weight = stuff[i][0] 
-        value = stuff[i][1]
-       
-        if j < weight:
-            knapsack[i][j] = knapsack[i - 1][j] #weight보다 작으면 위의 값을 그대로 가져온다
-        else:
-            knapsack[i][j] = max(value + knapsack[i - 1][j - weight], knapsack[i - 1][j])
+lst = [[0,0]] + [list(map(int, input().split())) for _ in range(number)]
 
-print(knapsack[N][K])
+knapsack = [ [0] * (bagWeight + 1) for _ in range(number + 1)]
+
+for i in range(1, number+1):
+    for j in range(1, bagWeight + 1):
+        
