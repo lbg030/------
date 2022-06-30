9a5073bb67bf2f0694d60932d987a7b094368311
ByeongGwon <lee971009@naver.com>
2022-06-30 14:42:02 +0900

    Gold 5

--- 16234.py
+++ 16234.py
@@ -1,23 +1,50 @@
-n,l,r = map(int, input().split())
+from collections import deque
 
-graph = [list(map(int,input().split())) for _ in range(n)]
+n, l, r = map(int, input().split())
+lst = [list(map(int, input().split())) for _ in range(n)]
 
-#옮겨야됨
-open = [[False] * n for _ in range(n)]
+cnt = 0
 
-dx = [0, 0, 1, -1]
-dy = [1,-1, 0, 0]
+dx = [1, -1, 0, 0]
+dy = [0, 0, -1, 1]
 
-while True :
+def bfs(i, j):
+    q = deque()
+    q.append([i, j])
+    result = []
+    result.append([i, j])
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if 0 <= nx < n and 0 <= ny < n and open[nx][ny] == 0:
+                if l <= abs(lst[nx][ny] - lst[x][y]) <= r:
+                    open[nx][ny] = 1    #open을 1로 지정함으로써 반복요소 제거
+                    q.append([nx, ny])
+                    result.append([nx, ny])
+    return result
+
+
+while True:
+    open = [[0] * n for i in range(n)]
+    flag = False
+    
     for i in range(n):
         for j in range(n):
-            for k in range(4):
+            if open[i][j] == 0:
+                open[i][j] = 1
+                result = bfs(i, j)
                 
-                x = i + dx[i]
-                y = j + dy[i]
-                
-                if 0 <= x < n and 0 <= y < n:
-                    if l < abs(graph[x][y] - graph[i][j]) < r :
-                        
-                        open[x][y] = open[y][x] = True #서로 연결되어있다는 의미
-                        
+                if len(result) > 1:
+                    flag = True
+                    
+                    num = sum([lst[x][y] for x, y in result]) // len(result)
+                    for x, y in result:
+                        lst[x][y] = num
+    if not flag:
+        break
+    cnt += 1
+    
+print(cnt)
