2a91e8c1d06dc62675b924d96cbeaf9dfff9de9c
ByeongGwon <lee971009@naver.com>
2022-06-30 23:48:13 +0900

    Gold 5 ( Knapsack 알고리즘)

--- 12865.py
+++ 12865.py
@@ -7,4 +7,11 @@ knapsack = [ [0] * (bagWeight + 1) for _ in range(number + 1)]
 
 for i in range(1, number+1):
     for j in range(1, bagWeight + 1):
+        value, weight = lst[i][1], lst[i][0]
         
+        if j < weight :
+            knapsack[i][j] = knapsack[i-1][j]
+        else :
+            knapsack[i][j] = max(value + knapsack[i-1][j-weight], knapsack[i-1][j])
+
+print(knapsack[number][bagWeight])
