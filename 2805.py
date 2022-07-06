51e892ff25d575360db4cbccce8c0475c1713275
ByeongGwon <lee971009@naver.com>
2022-07-06 18:21:07 +0900

    Silver 2

--- 2805.py
+++ 2805.py
@@ -2,7 +2,7 @@ from sys import stdin
 
 n, m = map(int, input().split())
 
-li = list(map(int, input().split()))
+li = list(map(int, stdin.readline().rstrip().split()))
 
 start, end = 1, max(li)  # 이분탐색 검색 범위 설정
 
