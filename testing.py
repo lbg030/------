b5a85689b5f40ca107250781e9abaa56e41a67c0
ByeongGwon <lee971009@naver.com>
2022-07-09 19:12:17 +0900

    Gold 4

--- testing.py
+++ testing.py
@@ -1,26 +1,44 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
+INF = int(1e9)
 
-n, k = map(int, input().split())
-visited = [False] * 100001
-q = deque()
-q.append([n, 0])
 
-min_count = sys.maxsize
+def bellman_ford(start):
+    dist[start] = 0
 
-while q:
-    cur, count = q.popleft()
-    visited[cur] = True
+    # n번의 라운드를 반복
+    for i in range(1, n + 1):
+        # 매 라운드마다 모든 간선을 확인
+        for j in range(m):
+            now, next, cost = edges[j][0], edges[j][1], edges[j][2]
+            # 현재 간선을 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
+            if dist[now] != INF and dist[next] > dist[now] + cost:
+                dist[next] = dist[now] + cost
+                # n번째 라운드에서도 값이 갱신된다면 음수 순환 존재
+                if i == n:
+                    return True
 
-    if cur == k:
-        min_count = min(min_count, count)
+    return False
 
-    if 0 <= cur + 1 < 100001 and not visited[cur + 1]:
-        q.append([cur + 1, count + 1])
-    if 0 <= cur - 1 < 100001 and not visited[cur - 1]:
-        q.append([cur - 1, count + 1])
-    if 0 <= cur * 2 < 100001 and not visited[cur * 2]:
-        q.append([cur * 2, count])
 
-print(min_count)
+n, m = map(int, input().split())
+edges = []
+dist = [INF] * (n + 1)
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    edges.append((a, b, c))
+
+negative_cycle = bellman_ford(1)
+
+if negative_cycle:
+    print(-1)
+else:
+    for i in range(2, n + 1):
+        # 도달할 수 없는 경우
+        if dist[i] == INF:
+            print(-1)
+        # 도달 가능한 경우
+        else:
+            print(dist[i])
+print(edges)
