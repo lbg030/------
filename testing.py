3d31de3ab6cb8a7625f35b024b01f438d425b736
ByeongGwon <lee971009@naver.com>
2022-07-10 17:00:52 +0900

    7/10

--- testing.py
+++ testing.py
@@ -1,44 +1,18 @@
-import sys
-input = sys.stdin.readline
-INF = int(1e9)
+from collections import deque
 
+T=int(1e5)+1
+N,K=map(int,input().split()) # 5 8
+Visit=[T for _ in' '*T]
 
-def bellman_ford(start):
-    dist[start] = 0
+D=deque([[N,0]]) # 5 0
+Visit[N]=0 #visit[5] 0
 
-    # n번의 라운드를 반복
-    for i in range(1, n + 1):
-        # 매 라운드마다 모든 간선을 확인
-        for j in range(m):
-            now, next, cost = edges[j][0], edges[j][1], edges[j][2]
-            # 현재 간선을 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
-            if dist[now] != INF and dist[next] > dist[now] + cost:
-                dist[next] = dist[now] + cost
-                # n번째 라운드에서도 값이 갱신된다면 음수 순환 존재
-                if i == n:
-                    return True
+while D:
+    [num,count]=D.popleft()
+    if Visit[num]>count:Visit[num]=count
+    if count<Visit[K]:
+        if 0<num*2<min(T,K*2) and Visit[num*2]>count:D.append([num*2,count])
+        if 0<num and Visit[num-1]>count+1:D.append([num-1,count+1])
+        if num<K and Visit[num+1]>count+1:D.append([num+1,count+1])
 
-    return False
-
-
-n, m = map(int, input().split())
-edges = []
-dist = [INF] * (n + 1)
-
-for _ in range(m):
-    a, b, c = map(int, input().split())
-    edges.append((a, b, c))
-
-negative_cycle = bellman_ford(1)
-
-if negative_cycle:
-    print(-1)
-else:
-    for i in range(2, n + 1):
-        # 도달할 수 없는 경우
-        if dist[i] == INF:
-            print(-1)
-        # 도달 가능한 경우
-        else:
-            print(dist[i])
-print(edges)
+print(Visit[K])
