1c3f140a953498f47be23fa2ddfbed1c2c860267
ByeongGwon <lee971009@naver.com>
2022-07-14 12:42:28 +0900

    Silver 2

--- 11725.py
+++ 11725.py
@@ -1,11 +1,30 @@
 from sys import stdin
 from collections import deque
 
+## 트리는 2차원 배열로 설정해야함
 n = int(input())
-graph = [[0] * (n+1) for _ in range(n+1)]
 
-for _ in range(n-1):
-    x, y = map(int, stdin.readline().rstrip().split())
-    graph[x][y] = graph[y][x] = 1
+graph = [[] for _ in range(n + 1)]
+parent = [0 for _ in range(n + 1)]
+
+for _ in range(n - 1):
+    a, b = map(int, stdin.readline().rstrip().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+# print(graph)
+# print(parent)
+
+def bfs(start):
+    q = deque()
+    q.append(start)
     
-    
+    while q :
+        v = q.popleft()
+        for i in graph[v]:
+            if parent[i] == 0:
+                parent[i] = v
+                q.append(i)
+bfs(1)
+for x in parent[2:]:
+    print(x)
