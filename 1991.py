a2d54a510ad13d4c2f6009b47572629014072292
ByeongGwon <lee971009@naver.com>
2022-07-16 21:43:30 +0900

    실버 1

--- 1991.py
+++ 1991.py
@@ -1,3 +1,35 @@
+from sys import stdin
+
 n = int(input())
 
-#tree left랑 tree right랑 나눠야하나 ?
+tree = {}
+
+for _ in range(n):
+    root,left, right = map(str, stdin.readline().strip().split())
+    tree[root] = [left,right]
+    
+# print(tree)
+# {'A': ['B', 'C'], 'B': ['D', '.'], 'C': ['E', 'F'], 'E': ['.', '.'], 'F': ['.', 'G'], 'D': ['.', '.'], 'G': ['.', '.']}
+def front(start):
+    if start != '.':
+        print(start, end='')
+        front(tree[start][0])
+        front(tree[start][1])
+        
+def middle(start):
+    if start != '.':
+        middle(tree[start][0])
+        print(start, end='')
+        middle(tree[start][1])  
+
+def back(start):
+    if start != '.':
+        back(tree[start][0])
+        back(tree[start][1])
+        print(start, end='')
+        
+front('A')
+print()
+middle('A')
+print()
+back('A')
