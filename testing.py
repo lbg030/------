821b68c99f7ddca24e457250229f72c1b59b516a
ByeongGwon <lee971009@naver.com>
2022-07-19 21:53:53 +0900

    code tree

--- testing.py
+++ testing.py
@@ -1,11 +1,35 @@
-n, m = map(int , input().split())
+n,m = map(int, input().split())
+graph = [[0] * m for _ in range(n)]
 
-lst = [[0] * n for _ in range(m)]
-
-k = 1
-for i in range(n):
-    for j in range(i,-1, -1):
-        lst[i][j] = k
-        k += 1
-
-print(lst)
+#시계 방향
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
+x,y = 0,0
+d = 0
+num = 1
+graph[0][0] = 1
+while True : 
+    if num >= n*m:
+        break
+    
+    nx = x + dx[d]
+    ny = y + dy[d]
+    
+    # print(nx,ny)
+    # print(graph)
+    # print(graph[nx][ny]) # 2 1
+    if 0<= nx < n and 0<= ny < m and not graph[nx][ny]:
+        num += 1
+        graph[nx][ny] = num
+        x,y = nx, ny
+    
+    else :
+        d += 1
+        
+    if d >= 4 :
+        d = d % 4
+        
+    
+    
+for x in graph:
+    print( *x)
