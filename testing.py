25fca4a014422c26c22fcd002825bdd631f125bd
ByeongGwon <lee971009@naver.com>
2022-07-20 21:57:39 +0900

    1

--- testing.py
+++ testing.py
@@ -1,35 +1,24 @@
-n,m = map(int, input().split())
-graph = [[0] * m for _ in range(n)]
+n,t = map(int, input().split())
 
-#시계 방향
-dx = [0,1,0,-1]
-dy = [1,0,-1,0]
-x,y = 0,0
-d = 0
-num = 1
-graph[0][0] = 1
-while True : 
-    if num >= n*m:
-        break
-    
-    nx = x + dx[d]
-    ny = y + dy[d]
-    
-    # print(nx,ny)
-    # print(graph)
-    # print(graph[nx][ny]) # 2 1
-    if 0<= nx < n and 0<= ny < m and not graph[nx][ny]:
-        num += 1
-        graph[nx][ny] = num
-        x,y = nx, ny
-    
+x,y,c=input().split()
+x,y = int(x), int(y)
+
+d = {'U':0, 'R': 1, 'D':2, 'L':3}
+
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
+
+d_num = d[c]
+
+for _ in range(t):
+    if 1<= x <= n or 1 <= y <= n:
+        x = x + dx[d_num]
+        y = y + dy[d_num]
+
     else :
-        d += 1
+        d_num = (d_num + 2) % 4
+        x = x + dx[d_num]
+        y = y + dy[d_num]
         
-    if d >= 4 :
-        d = d % 4
-        
-    
-    
-for x in graph:
-    print( *x)
+    print(x,y)
+# print(x,y)
