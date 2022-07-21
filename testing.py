216c1b4619c2e62cd79e7f69504f2261da08e385
ByeongGwon <lee971009@naver.com>
2022-07-21 19:34:54 +0900

    testing

--- testing.py
+++ testing.py
@@ -1,24 +1,4 @@
-n,t = map(int, input().split())
+n = int(input())
 
-x,y,c=input().split()
-x,y = int(x), int(y)
+lst = [list(map(int, input().split())) for _ in range(n)]
 
-d = {'U':0, 'R': 1, 'D':2, 'L':3}
-
-dx = [-1,0,1,0]
-dy = [0,1,0,-1]
-
-d_num = d[c]
-
-for _ in range(t):
-    if 1<= x <= n or 1 <= y <= n:
-        x = x + dx[d_num]
-        y = y + dy[d_num]
-
-    else :
-        d_num = (d_num + 2) % 4
-        x = x + dx[d_num]
-        y = y + dy[d_num]
-        
-    print(x,y)
-# print(x,y)
