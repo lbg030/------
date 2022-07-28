30fb6c8daf0af862cd960225833315f18e13865e
ByeongGwon <lee971009@naver.com>
2022-07-28 15:07:59 +0900

    Gold 5

--- 1107.py
+++ 1107.py
@@ -1,6 +1,6 @@
 n = int(input())
 m = int(input())
-limit = 1000001
+limit = 1000001 ## 511111에 접근하려고 하는데 0,1,2,3,4 가 broken 이라면  555555 에서 접근해야 되기 때문
 res = abs(100 - n)
 
 if m :
@@ -8,6 +8,9 @@ if m :
 else :
     broken = set()
     
+    
+#for else 구문 :
+    # for 문에서 break가 걸리지 않았다면 else문 으로 이동
 for i in range(limit):
     for x in str(i):
         if x in broken:
