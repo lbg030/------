fd68964690e242cf4ed13d4caca29c6585e51e09
ByeongGwon <lee971009@naver.com>
2022-07-30 14:22:03 +0900

    Gold 4

--- 2206.py
+++ 2206.py
@@ -15,31 +15,31 @@ dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 
 
-def bfs(x, y, z):
+def bfs():
     queue = deque()
-    queue.append((x, y, z))
+    queue.append((0,0,0))
 
     while queue:
-        a, b, c = queue.popleft()
+        x,y,cnt = queue.popleft()
         
         # 끝 점에 도달하면 이동 횟수를 출력
-        if a == n - 1 and b == m - 1:
-            return visited[a][b][c]
+        if x == n - 1 and y == m - 1:
+            return visited[x][y][cnt]
         
         for i in range(4):
-            nx = a + dx[i]
-            ny = b + dy[i]
+            nx = x + dx[i]
+            ny = y + dy[i]
             if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
             # 다음 이동할 곳이 벽이고, 벽파괴기회를 사용하지 않은 경우
-            if graph[nx][ny] == 1 and c == 0 :
-                visited[nx][ny][1] = visited[a][b][0] + 1
+            if graph[nx][ny] == 1 and cnt == 0 :
+                visited[nx][ny][1] = visited[x][y][0] + 1
                 queue.append((nx, ny, 1))
             # 다음 이동할 곳이 벽이 아니고, 아직 한 번도 방문하지 않은 곳이면
-            elif graph[nx][ny] == 0 and visited[nx][ny][c] == 0:
-                visited[nx][ny][c] = visited[a][b][c] + 1
-                queue.append((nx, ny, c))
+            elif graph[nx][ny] == 0 and visited[nx][ny][cnt] == 0:
+                visited[nx][ny][cnt] = visited[x][y][cnt] + 1
+                queue.append((nx, ny, cnt))
     return -1
 
-print(bfs(0, 0, 0))
+print(bfs())
 # print(visited)
