c852ae9f1c786afc6466dc13e453ea8e5d134e53
ByeongGwon <lee971009@naver.com>
2022-08-05 17:30:02 +0900

    Gold 4

--- 11559.py
+++ 11559.py
@@ -1,7 +1,52 @@
 from collections import deque
 
 field = [list(input()) for _ in range(12)]
+visited = [[False] * 6 for _ in range(12)]
+flag = 1
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+res = 0
 
-for i in range(12):
-    for j in range(6):
-        
+def bfs(i,j):
+    color = field[i][j]
+    q = deque((i,j))
+    removed = [i,j]
+    cnt = 1
+    visited[i][j] = True
+    
+    while q:
+        x,y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < 12 and 0<= ny < 6:
+                if field[nx][ny] == color and visited[nx][ny] == False:
+                    q.append((nx,ny))
+                    visited[nx][ny] = True
+                    removed.append((nx,ny))
+                    cnt += 1
+    if cnt >= 4:
+        for x,y in removed:
+            field[x][y] = '.'
+            return True
+    
+    else :
+        return False
+
+
+while flag:
+    flag = 0
+    for i in range(12):
+        for j in range(6):
+            if field[i][j] != '.':
+                flag = bfs(i,j)
+                if flag :
+                    res += 1
+                    
+
+    for i in range(11):
+        for j in range(6):
+            if field[i][j] != '.' and field[i+1][j] == '.':
+                field[i+1][j] == field[i][j]
+                field[i][j] = '.'
+                
