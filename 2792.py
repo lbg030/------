cc37b26c8c767622944561e65fcf939eed44c7b2
ByeongGwon <lee971009@naver.com>
2022-08-07 23:49:02 +0900

    Silver 2

--- 2792.py
+++ 2792.py
@@ -1,6 +1,31 @@
 # 학생은 항상 같은 색상의 보석을 가짐
-n, m = map(int, input().split()) # n의 학생 m의 색상
+n, m = map(int, input().split()) # n =  학생 / m =  색상
 lst = []
 for _ in range(m):
     lst.append(int(input()))
     
+minimum = 1
+maximum = sum(lst)
+
+while minimum <= maximum:
+    mid = (minimum + maximum) // 2
+    
+    if mid == 0 :
+        break
+    
+    student = 0
+    
+    for x in lst :
+        if x % mid > 0 :
+            student += x // mid + 1
+        else :
+            student += x // mid
+        
+    if student > n:
+        minimum = mid + 1
+        
+    else :
+        maximum = mid - 1
+        ans = mid
+
+print(ans)
