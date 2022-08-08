ecbe66fc014d0b2331700bd794320251947da521
ByeongGwon <lee971009@naver.com>
2022-08-08 13:31:11 +0900

    Gold 4

--- 1477.py
+++ 1477.py
@@ -1,21 +1,33 @@
-n,m,l = map(int, input().split())
-lst = [0] + list(map(int, input().split())) + [l]
-lst.sort()
+# 리스트 안에 집어넣어 계산하면 한 지점부터 다른 지점까지에 대해 2개는 가능하지만 3개 이상으로 가면 3분할 4분할이 되지 않음
+# 리스트 append 대신 값 자체를 찾는 방식으로 생각
 
-start, end = 1, l-1
+n,m,l = map(int, input().split())
+# n이 0일수도 있기 때문에 예외 처리
+if n :
+    lst = [0] + list(map(int, input().split())) + [l]
+else :
+    lst = [0,l]
+
+lst.sort()
+start,end = 1,l # start = 0 으로 하면 특정 케이스에서 zerodivisionerror 발생
 res = 0
+
 while start <= end:
-    count = 0
-    mid = (start+end) // 2
-    for i in range(1, len(lst)):
-        # 현재 거리 중 mid보다 큰 거리를 찾아서
-        if lst[i]-lst[i-1] > mid:
-            # 나눈 만큼 휴게소를 설치 (현재 설치 돼있는 구역은 제외해야해서 -1)
-            count += (lst[i]-lst[i-1]-1)//mid
-    if count > m:
-        start = mid+1
-    else:
-        end = mid-1
-        result = mid
-print(result)
- 
+    cnt = 0 # m을 만족하는지 만족 못하는지 점검
+    mid = (start + end) // 2
+    
+    for i in range(len(lst)-1):
+        if lst[i+1]- lst[i] > mid:
+            cnt += (lst[i+1] - lst[i]) // mid
+            
+            # 딱 나누어 떨어진 경우에 이미 세워진 곳에 하나를 더 세우는 것이기 때문에 빼줘야 한다.
+            if (lst[i+1]- lst[i]) % mid == 0:
+                cnt -= 1
+            
+    if cnt > m:
+        start = mid + 1
+    else :
+        end = mid - 1
+        res = mid
+        # print(res)
+print(res)
--- testing.py
+++ testing.py
@@ -1,64 +1,20 @@
-from collections import deque
+N, M, L = map(int, input().split())
+arr = [0]+list(map(int, input().split()))+[L]
+arr.sort()
 
-field = [list(input()) for _ in range(12)]
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-res = 0 # 결과
-
-flag = 1 # while 문을 돌리기 위한 초기값
-
-def bfs(i,j):
-    color = field[i][j]
-    q = deque()
-    q.append((i,j))
-    removed = [(i,j)] #만약 길이가 4 이상이면 revmoed에 있는 좌표값으로 field에서 제거
-    visited[i][j] = True
-    
-    while q:
-        x,y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < 12 and 0<= ny < 6: # 인덱스
-                if field[nx][ny] == color and visited[nx][ny] == False: #색깔이 같고 방문하지 않은 곳이라면
-                    q.append((nx,ny))
-                    visited[nx][ny] = True
-                    removed.append((nx,ny))
-    
-    if len(removed) >= 4:
-        for x,y in removed:
-            field[x][y] = '.'
-        return True
-    
-    # 길이가 4 미만
-    else :
-        return False
-
-
-while flag > 0:
-    flag = 0 # 변하지 않았다란 의미로 0으로 설정
-    visited = [[False] * 6 for _ in range(12)] #visit은 while한번 할때마다 초기화 시켜줘야하기 때문에 여기에 위치
-    for i in range(12):
-        for j in range(6):
-            if field[i][j] != '.':
-                flag += bfs(i,j) # True면 +1 False면 0 
-                
-    # 중력에 따라 내려가게 하기
-    # 처음에는 위에서 아래로 하였으나 이렇게 하면 오류가 발생하게 되어 거꾸로 하는 것으로 변경
-    for i in range(10,-1,-1):
-        for j in range(5,-1,-1):
-            if field[i][j] != '.' and field[i+1][j] == '.':  #특정 좌표가 . 이 아닌데 그 아래는 .인 경우 ( 중력을 받아야 하는 경우)
-                for k in range(1,11-i+1): # 해당 점부터 맨 끝까지 탐색 
-                    if field[i + k][j] != '.':
-                        field[i+k-1][j] = field[i][j]
-                        field[i][j] = '.'
-                        break
-                    
-                    elif i+k == 11: # 예외처리 ( 마지막 까지 . 인 경우 )
-                        field[i+k][j] = field[i][j]
-                        field[i][j] = '.'
-                            
-    if flag > 0 :
-        res += 1
-    
-print(res)
+start, end = 1, L-1
+result = 0
+while start <= end:
+    count = 0
+    mid = (start+end) // 2
+    for i in range(1, len(arr)):
+        # 현재 거리 중 mid보다 큰 거리를 찾아서
+        if arr[i]-arr[i-1] > mid:
+            # 나눈 만큼 휴게소를 설치 (현재 설치 돼있는 구역은 제외해야해서 -1)
+            count += (arr[i]-arr[i-1]-1)//mid
+    if count > M:
+        start = mid+1
+    else:
+        end = mid-1
+        result = mid
+print(result)
