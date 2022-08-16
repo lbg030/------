0946f4b0ea1f252666dac9eb491c54f44b34b6f2
ByeongGwon <lee971009@naver.com>
2022-08-17 00:24:45 +0900

    Silver 4

--- 17419.py
+++ 17419.py
@@ -1,9 +1,9 @@
 #  K = K-(K&((~K)+1))
+
 a = int(input())
+
 binary_k = '0b' + input()
 cnt = 0
-decimal_k = int(binary_k[2:], 2)
-
 def not_binary(n):
     k = ''
     for i in range(2, len(n)):
@@ -15,19 +15,18 @@ def not_binary(n):
 
 def decimal_to_binary(n):
     return bin(n)
+        
+
 
 while True:
-    binary_k = bin(decimal_k)
-    middle = (decimal_k & ((not_binary(binary_k) + 1)))
-    res = decimal_k - middle
+    k = (((not_binary(bin(a))) + 1))
+    middle = a&k
     
-    if res == 0 :
-        cnt += 1
+    res = a - middle
+    print(res)
+    if res == 0:
         print(cnt)
         break
-    
     else :
         cnt += 1
-        decimal_k = res
-        # print(res)
-        
+        a = res
