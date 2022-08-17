e6e9fe46766a5e16f2d30bbab74cb3736bf5f77b
ByeongGwon <lee971009@naver.com>
2022-08-17 21:24:29 +0900

    Gold 4

---
+++ 15661.py
@@ -0,0 +1,25 @@
+from itertools import combinations
+ 
+N = int(input())
+ 
+ 
+arr = [list(map(int,input().split())) for _ in range(N)]
+row = [sum(i) for i in arr]
+col = [sum(i) for i in zip(*arr)]
+
+new_arr = [i+ j for i, j in zip(row, col)]
+total_sum = sum(new_arr)//2
+result = float('inf')
+
+for num in range(1,N//2+1):
+    for combi in combinations(new_arr,num):
+        result = min(result,abs(total_sum-sum(combi)))
+        if result == 0:
+            break
+    if result == 0:
+        break
+print(result)
+
+print(row, col)
+
+print(new_arr)
