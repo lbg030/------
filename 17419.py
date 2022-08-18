4a78f84b1954269d3224833df72ccced6c7cdf80
ByeongGwon <lee971009@naver.com>
2022-08-18 19:15:18 +0900

    Silver 4

--- 17419.py
+++ 17419.py
@@ -3,30 +3,7 @@
 a = int(input())
 
 binary_k = '0b' + input()
+decimal_k = int(binary_k,2)
 cnt = 0
-def not_binary(n):
-    k = ''
-    for i in range(2, len(n)):
-        if n[i] == '1' :
-            k += '0'
-        else :
-            k += '1'
-    return int(k,2)
 
-def decimal_to_binary(n):
-    return bin(n)
-        
-
-
-while True:
-    k = (((not_binary(bin(a))) + 1))
-    middle = a&k
-    
-    res = a - middle
-    print(res)
-    if res == 0:
-        print(cnt)
-        break
-    else :
-        cnt += 1
-        a = res
+print(decimal_k)
--- testing.py
+++ testing.py
@@ -1,54 +1,6 @@
-from math import gcd
+a = '0b1110' # 14
+b = 0b11110 
 
-n = int(input())
-result = [1]*n
-graph = {}
+a = bin(a)
 
-def gcd_list(lst):
-    divided = lst[0]
-    for x in lst:
-        divided = gcd(divided, x)
-    return divided
-
-def dfs(a, b, edge):
-    global visited
-    
-    # 이미 한 것이라면 pass
-    if (a, edge) in visited: 
-        return
-    
-    # pass가 안됐으면 아직 값 업데이트가 되지 않은 것.
-    visited[(a, edge)] = True
-    result[a] *= edge
-    
-    for x in graph[a]:
-        if x != b: 
-            dfs(x, a, edge)
-
-
-cocktail_list=[]
-
-for _ in range(n-1):
-    a, b, p, q = map(int, input().split())
-    
-    if a not in graph:
-        graph[a] = [b]
-    else :
-        graph[a].append(b)
-        
-    if b not in graph:
-        graph[b] = [a]
-    else :
-        graph[b].append(a)
-        
-    cocktail_list.append((a, b, p, q))
-
-for a,b,p,q in cocktail_list:
-    visited={}
-    dfs(a, b, p)
-    dfs(b, a, q)
-    # print(visited)
-divided = gcd_list(result)
-result = list(map(lambda x: x // divided , result))
-
-print(*result)
+print(a)
