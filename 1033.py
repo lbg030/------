7f8cd3aff590ec60d4eb5dc9fc440efcedf52137
ByeongGwon <lee971009@naver.com>
2022-08-18 14:58:04 +0900

    Gold 2

--- 1033.py
+++ 1033.py
@@ -6,8 +6,7 @@ def gcd(a, b):
 
 # 최소 공배수
 def lcm(a, b):
-    return a * b / gcd(a, b)
-
+    return a * b // gcd(a, b)
 
 n = int(input())
 lst = [1] * (n)
@@ -17,7 +16,8 @@ graph = {}
 
 for i in range(n-1):
     a,b,p,q = map(int, input().split())
-    cocktail_list.append((a,b,p,q))
+
+    divided = gcd(p,q) # 기약분수로 만들기 위해
     
     if a not in graph:
         graph[a] = [b]
@@ -29,16 +29,44 @@ for i in range(n-1):
     else :
         graph[b].append(a)
         
-for a,b,p,q in cocktail_list:
-    divided = gcd(p,q) # 기약분수로 만들기 위해
+        
     if divided != 1:
         p = p // divided
         q = q // divided
         
+    if lst[a] == 1 and lst[b] == 1:
+        # print("LCM",a,b,p,q)
+        prev_a = lst[a]
+        prev_b = lst[b]
+        
+        lst[a] = prev_b * p
+        lst[b] = prev_a * q
+        
+        
+        for x in graph[a]:
+            if x != b:
+                lst[x] *= p
+        
+        for x in graph[b]:
+            if x != a:
+                lst[x] *= q
     
+    else :
+        lcm_value = lcm(lst[a], lst[b])
+        
+        prev_a = lst[a]
+        lst[a] = lcm_value * p
+        for x in graph[a]:
+            if x != b:
+                lst[x] *= (lst[a] // prev_a)
+        
+        prev_b = lst[b]
+        lst[b] = lcm_value * q
+        for x in graph[b]:
+            if x != a:
+                lst[x] *= (lst[b] // prev_b)
+            
+print(*lst)
+print(graph)
 
 
-# 1 1 3 4
-# 3 1 3 4
-# 3과 4의 최소공배수 * 5 와 *7 - > 12 * 5 ; 12 * 7
-# 60 20 63 84
