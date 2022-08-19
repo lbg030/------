fba7ff465252bdaebda23c3f6aeff9161c2ed373
ByeongGwon <lee971009@naver.com>
2022-08-19 15:34:41 +0900

    Gold 4

--- 1987.py
+++ 1987.py
@@ -1,5 +1,6 @@
+from sys import stdin
 n, m = map(int, input().split())
-graph = [list(input()) for _ in range(n)]
+graph = [list(map(str, stdin.readline().rstrip())) for _ in range(n)]
 
 dx = [0, 0, 1, -1]
 dy = [1,-1, 0, 0]
@@ -21,5 +22,6 @@ def dfs(x,y,cnt):
                 dfs(nx,ny,cnt + 1)
                 # dfs 가 끝나면 visited에 있던 항목들을 dfs하기 전으로 되돌려야 하기 때문에 remove
                 del(dic[str(graph[nx][ny])]) 
+
 dfs(0,0,1)
 print(res)
--- testing2.py
+++ testing2.py
@@ -1,3 +1,25 @@
-n = 99
+n, m = map(int, input().split())
+graph = [list(input()) for _ in range(n)]
 
-print((n*(n+1)) // 2)
+dx = [0, 0, 1, -1]
+dy = [1,-1, 0, 0]
+
+res = 0
+dic = set()
+dic.add(graph[0][0])
+
+
+def dfs(x,y,cnt):
+    global res
+    res = max(res, cnt)
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        
+        if 0<= nx < n and 0 <= ny < m and str(graph[nx][ny]) not in dic:
+                dic.add(graph[nx][ny])
+                dfs(nx,ny,cnt + 1)
+                # dfs 가 끝나면 visited에 있던 항목들을 dfs하기 전으로 되돌려야 하기 때문에 remove
+                dic.remove(graph[nx][ny])
+
+dfs(0,0,1)
+print(res)
