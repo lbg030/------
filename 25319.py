3f44af53e4caee4dba0a135347dd858f569c41ef
ByeongGwon <lee971009@naver.com>
2022-08-24 21:34:37 +0900

    Gold 5

--- 25319.py
+++ 25319.py
@@ -1,84 +1,64 @@
+from collections import deque
 n,m,s = map(int, input().split())
 
 graph = [list(input()) for _ in range(n)]
 target = input()
-x, y = 0,0
+alpha_dic = {}
+
 res = ''
 cnt = 0
-final_res = ''
-reserve_x, reserve_y = 0,0
-flag = True
 
-def left_or_right(lst, alpha, y):
+for x in target:
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == x:
+                if x in alpha_dic:
+                    alpha_dic[x].append((i, j))
+                else :
+                    alpha_dic[x] = deque()
+                    alpha_dic[x].append([i, j])
+                    
+def direction(x,y,final = False):
     ans = ''
-    value = lst.index(alpha) - y 
-    if value < 0 :
-        ans += 'L' * abs(value)
-    
+    if x < 0: # go_x < x
+        ans += 'U' * abs(x)
     else :
-        ans += 'R' * abs(value)
+        ans += 'D' * abs(x)
+        
+    if y < 0: # go_y < y
+        ans += 'L' * abs(y)
+    else :
+        ans += 'R' * abs(y)
     
-    ans += 'P'
+    if not final:
+        ans += 'P'
+    
     return ans
 
+x, y = 0,0
 
-while flag:
-    res = ''
+while True :
+    temp_res = ''
     for alpha in target:
-        flag = False
-        
-        if alpha in graph[x]:
-            y = graph[x].index(alpha)
-            res += left_or_right(graph[x], alpha, y)
-            graph[x][y] = '.'
+        if alpha_dic[alpha]:
+            go_x, go_y = alpha_dic[alpha].popleft()
+            # print(type(temp_res),type(x),type(go_x))
+            temp_res += direction(go_x - x, go_y - y)
+            x,y = go_x,go_y
             flag = True
             
-        else:
-            for i in range(1, max(n-x, x)):
-                plus = x + i
-                minus = x - i
-                
-                # x보다 밑에 있음
-                if plus < n:
-                    if alpha in graph[plus]:
-                        res += 'D' * (plus - x)
-                        res += left_or_right(graph[plus], alpha, y)
-                        
-                        x = plus
-                        y = graph[plus].index(alpha)
-                        graph[x][y] = '.'
-                        flag = True
-                        continue
-                    
-                elif 0 < minus :
-                    if alpha in graph[minus]:
-                        res += 'U' * (x - minus)
-                        res += left_or_right(graph[minus], alpha, y)
-                            
-                        x = minus
-                        y = graph[minus].index(alpha)
-                        graph[x][y] = '.'
-                        res += 'P'
-                        flag = True
-                        continue
-                    
-            if not flag:
-                break
+        else :
+            flag = False
+            break
         
-    if flag:
-        final_res += res
+    if flag :
+        res += temp_res
+        final_x,final_y = x,y
         cnt += 1
-        reserve_x = x
-        reserve_y = y
         
     else :
-        # print(final_res, x,y)
-        final_x = n - reserve_x - 1
-        final_y = m - reserve_y - 1
-        
-        final_res += 'D' * final_x
-        final_res += 'R' * final_y
-        # print(final_res)
-        
-print(cnt,len(final_res))
-print(final_res)
+        res += direction(n-x-1, m-y-1, True)
+        break
+
+print(cnt, len(res))
+print(res)
--- testing2.py
+++ testing2.py
@@ -1,15 +1,23 @@
+from collections import deque
 n,m,s = map(int, input().split())
+
 graph = [list(input()) for _ in range(n)]
 target = input()
+alpha_dic = {}
 
-def left_or_right(lst, alpha, y):
-    ans = ''
-    value = lst.index(alpha) - y 
-    if value < 0 :
-        ans += 'L' * abs(value)
-    
-    else :
-        ans += 'R' * abs(value)
-    
-    ans += 'P'
-    return ans
+res = ''
+cnt = 0
+
+for x in target:
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == x:
+                if x in alpha_dic:
+                    alpha_dic[x].append([i, j])
+                else :
+                    alpha_dic[x] = deque()
+                    alpha_dic[x].append([i, j])
+                    
+x,y = alpha_dic['c'].popleft()
+
+print(x,y,type(x))
