091445ba03d85b26a3d7a394a04fb22541dd4e27
ByeongGwon <lee971009@naver.com>
2022-09-12 18:12:29 +0900

    Silver 1

--- 1911.py
+++ 1911.py
@@ -2,19 +2,31 @@ from collections import deque
 import math
 
 N, L = map(int, input().split())
-
 cnt = 0 
 lst= []
-
 
 for _ in range(N):
     a,b = map(int, input().split())
     lst.append((a,b))
     
 lst.sort()
-location = lst[0][0]
+loc = lst[0][0]
 lst= deque(lst)
 
 while lst:
     a,b = lst.popleft()
     
+    if b < loc :
+        continue
+    
+    elif loc < a:
+        loc = a
+    # continue에 걸리지 않았으므로 무조건 한개 이상 깔아야함
+    if (b - loc) % L == 0 :
+        cnt += (b-loc) // L
+        loc += L * ((b-loc)//L)
+    else :
+        cnt += math.ceil((b-loc)/L)
+        loc += L * math.ceil((b-loc)/L)
+        
+print(cnt)
--- testing.py
+++ testing.py
@@ -1,1 +1,17 @@
-print("testing")
+n, l = map(int, input().split())
+pot = [list(map(int, input().split())) for _ in range(n)]
+pot.sort()
+cur = pot[0][0]
+answer = 0
+for s, e in pot:
+    if cur > e:
+        continue
+    elif cur < s:
+        cur = s
+    if (e-cur)%l == 0:
+        answer += (e-cur)//l
+        cur += ((e-cur)//l) * l
+    else:
+        answer += (e-cur)//l + 1
+        cur += ((e-cur)//l + 1) * l
+print(answer)
