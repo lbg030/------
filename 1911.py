e58d96e9dc292fd0bf5fa50b384475768f448ab2
ByeongGwon <lee971009@naver.com>
2022-09-12 17:53:06 +0900

    Silver 2

--- 1911.py
+++ 1911.py
@@ -1,6 +1,20 @@
+from collections import deque
+import math
+
 N, L = map(int, input().split())
 
 cnt = 0 
+lst= []
+
 
 for _ in range(N):
     a,b = map(int, input().split())
+    lst.append((a,b))
+    
+lst.sort()
+location = lst[0][0]
+lst= deque(lst)
+
+while lst:
+    a,b = lst.popleft()
+    
