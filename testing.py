b8dc3c52518d1edf99cfd9cd611c1a9a3e0097ca
병권 <66514138+lbg030@users.noreply.github.com>
2022-09-14 23:05:18 +0900

    testing

--- testing.py
+++ testing.py
@@ -1,8 +1,1 @@
-import wandb
-wandb.init(settings=wandb.Settings(start_method="fork"))
-
-for x in range(10):
-    run = wandb.init(reinit=True)
-    for y in range (100):
-        wandb.log({"metric": x+y})
-    run.finish()
+print(testing)
