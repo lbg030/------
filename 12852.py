d01cbff42e398af805d875c358ffeb6629cbfb1b
ByeongGwon <lee971009@naver.com>
2022-09-18 12:42:25 +0900

    Silver 1

--- 12852.py
+++ 12852.py
@@ -1,27 +1,27 @@
-n = int(input())
+from collections import deque
 
-ans = [n]
-
-while n != 1:
-    if (n-1) % 3 == 0:
-        n = n - 1
-        ans.append(n)
-        n //= 3
-        ans.append(n)
-        
-    elif (n-1) % 2 == 0:
-        if n % 3 == 0:
-            n //= 3
-            ans.append(n)
-        else :
-            n = n - 1
-            ans.append(n)
-            n //= 2
-            ans.append(n)
+def bfs(number, res_lst):
+    q = deque()
+    q.append((number, res_lst))
     
-    else :
-        n = n - 1
-        ans.append(n)
+    while q:
+        number, res_lst = q.popleft()
+        # print(number, res_lst)
+        for n in [number+1, number*2, number*3]:
+            if n <= N and lst[n] == 0:
+                if n == N:
+                    return lst[number]+1, res_lst+[n]
+                
+                q.append((n, res_lst+[n]))
+                lst[n] = lst[number]+1
 
-print(len(ans)-1)
-print(*ans)
+N = int(input())
+if N == 1:
+    print(0)
+    print(1)
+else:
+    lst = [0]*(N+1)
+    
+    cnt, res_lst = bfs(1, [1])
+    print(cnt)
+    print(*res_lst[::-1])
--- testing.py
+++ testing.py
@@ -1,1 +1,26 @@
-print((9 * 0.6))
+from collections import deque
+
+def bfs(node, dp):
+    q = deque()
+    q.append((node, dp))
+    
+    while q:
+        node, dp = q.popleft()
+        
+        for n in [node+1, node*2, node*3]:
+            if n <= N and check[n] == 0:
+                if n == N:
+                    return check[node]+1, dp+[n]
+                
+                q.append((n, dp+[n]))
+                check[n] = check[node]+1
+
+N = int(input())
+if N == 1:
+    print(0)
+    print(1)
+else:
+    check = [0]*(N+1)
+    cnt, dp = bfs(1, [1])
+    print(cnt)
+    print(*dp[::-1])
