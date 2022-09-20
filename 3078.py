03f4c82302e2f5464e2bf9b1c0c94cf200f75328
ByeongGwon <lee971009@naver.com>
2022-09-20 15:44:22 +0900

    play

--- 3078.py
+++ 3078.py
@@ -1,23 +1,3 @@
-from collections import deque
-
-n, k = map(int, input().split())
-
-good_friends = 0
-
-lst = [len(input()) for _ in range(n)]
-i = 1
-compared_list = deque(lst[:k+i])
-# print(compared_list)
-
-while compared_list :
-    i += 1
-    length = (compared_list.popleft())
-    good_friends += compared_list.count(length)
-    
-    if k + i <= n:
-        compared_list.append(lst[-1+k+i])
-    # print(compared_list)    
-print(good_friends)
-    
-    
-    
+lst = [0, 103, 88, 91, 99, 101, 86, 96, 100, 71, 91, 96, 91, 98, 102, 96, 85, 102, 97, 91, 112, 85, 79, 86, 91, 97, 98, 110, 80, 88, 80, 95, 83, 93, 109, 88, 91, 103, 90, 95, 113, 81, 86, 97, 93, 93]
+n = int(input())
+print(lst[n])
--- testing.py
+++
@@ -1,4 +0,0 @@
-import random
-
-n = random.randint(0,1)
-print('Korea' if n >= 0.5 else 'Yonsei')
