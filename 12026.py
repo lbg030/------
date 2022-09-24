b0ff176a027e24b30d4bafba074e65a0984e6a18
ByeongGwon <lee971009@naver.com>
2022-09-24 18:46:35 +0900

    Silver 1

--- 12026.py
+++ 12026.py
@@ -1,4 +1,21 @@
 n = int(input())
-boj_list = input()
-for idx, value in enumerate(boj_list):
-    pass
+lst = input()
+
+inf = 1e9
+dp = [inf] * n
+
+def get_prev(x):
+    if x == "B":
+        return "J"
+    elif x == "O":
+        return "B"
+    elif x == "J":
+        return "O"
+
+dp[0] = 0
+for i in range(1, n):
+    prev = get_prev(lst[i])
+    for j in range(i):
+        if lst[j] == prev:
+            dp[i] = min(dp[i], dp[j] + pow(i - j, 2))
+print(dp[n - 1] if dp[n - 1] != inf else -1)
