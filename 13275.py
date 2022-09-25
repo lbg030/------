7acd7fd76468411a0c13bfcd2f4c39f43e2f8809
ByeongGwon <lee971009@naver.com>
2022-09-25 12:45:49 +0900

    Platinum 5

--- 13275.py
+++ 13275.py
@@ -1,1 +1,16 @@
 #매내쳐 알고리즘 O(n)
+from collections import deque
+
+s = list(input())
+res = 1
+
+# s가 짝수일 때
+if len(s) % 2 == 0:
+    pointer = 0
+    left_s = deque()
+    right_s = deque(s[:])
+    
+    string = (right_s.popleft())
+    
+    for i in range(min(len(left_s), len(right_s))):
+        
