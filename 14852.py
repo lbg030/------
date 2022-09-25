bf3750066a9325b662fd7a4eb262be14a3f73306
ByeongGwon <lee971009@naver.com>
2022-09-25 11:37:27 +0900

    Gold 4

--- 14852.py
+++ 14852.py
@@ -1,12 +1,23 @@
+from collections import deque
 
 n = int(input())
-
+divided = 1000000007
 # 0 1 2 3 4
 # 1 2 7 22 71
 
+# 7 * 3 + 2 - 1
 # 22 * 3 + 7 - 2
 
-# lst = [0] * (n+1)
-# lst[1] = 2
-# lst[2] = 7
+lst = deque([1,2,7])
+
 # lst[3] = 22
+if n >= 3:
+    for _ in range(3, n+1):
+        lst.append((lst[2] * 3 + lst[1] - lst[0])  % divided)
+        lst.popleft()
+
+    print(lst[-1])
+else :
+    print(lst[n])
+
+    
