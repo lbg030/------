fb43424b71cb7a04c71b77113df16630f3c56654
ByeongGwon <lee971009@naver.com>
2022-10-05 12:36:08 +0900

    1

--- testing.py
+++ testing.py
@@ -1,1 +1,11 @@
-print(testing)
+import sys
+input=sys.stdin.readline
+from collections import defaultdict
+
+N,K=map(int,input().split())
+array=[len(input()) for _ in range(N)]
+dic=defaultdict(list)
+for i in range(N):
+    dic[array[i]].append(i)
+print(array)
+print(dic[4])
