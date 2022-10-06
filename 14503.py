f74571faaa81de2b7b45054c575e06f64dcc18ed
병권 <66514138+lbg030@users.noreply.github.com>
2022-10-06 22:26:12 +0900

    Gold 5

--- 14503.py
+++ 14503.py
@@ -5,92 +5,37 @@ robot = list(map(int, input().split()))
 graph = [list(map(int, input().split())) for _ in range(N)]
 visited = [[False] * M for _ in range(N)]
 
-clean = 0
-x ,y, direction = robot[0],robot[1],robot[2]
+x, y, direction = robot[0],robot[1],robot[2]
 
 # 왼쪽으로 회전
-def left_move(x,y,direction):
-    if direction == 0:
-        return x,y-1,3
-    
-    elif direction == 1:
-        return x-1,y,0
-    
-    elif direction == 2:
-        return x, y+1,1
-    
-    else :
-        return x+1,y,2
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
 
-def back(x,y,direction):
-    if direction == 0:
-        return x+1,y,direction
-    
-    elif direction == 1:
-        return x,y-1,direction
-    
-    elif direction == 2:
-        return x-1,y,direction
-    
-    else :
-        return x,y+1,direction
+visited[x][y] = True
+cnt = 1
 
-def bounding_check(x,y, direction = None):
-    if 0 <= x < N and 0 <= y < M :
-        return True
-    else :
-        return False
-    
-def cleaning_check(x,y,direction = None):
-    if graph[x][y] == 0 and visited[x][y] == False:
-        return True
-    else :
-        return False
-    
-    return 
 while True:
-    # 청소할 상황이 있는 경우 
-    print(x,y)
+    flag = False
     
-    clean += 1
-    visited[x][y] = True
-    temp_x,temp_y,temp_direction = left_move(x,y,direction)
+    for _ in range(4):
+        nx = x + dx[(direction + 3) % 4]
+        ny = y + dy[(direction + 3) % 4]
+        
+        direction = (direction + 3) % 4
+        
+        if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 0 and visited[nx][ny] == False:
+            visited[nx][ny] = True
+            cnt += 1
+            x = nx
+            y = ny
+            
+            flag = True
+            break
     
-    if bounding_check(temp_x,temp_y,direction) and cleaning_check(temp_x, temp_y, direction):
-        x,y,direction = temp_x, temp_y, temp_direction
-
-    else :
-        num = 0
-        while num < 4:
-            if num == 1:
-                temp_x, temp_y, temp_direction = left_move(x,y,direction)
-            else :
-                temp_x, temp_y, temp_direction = left_move(x,y,temp_direction)
-            
-            print(temp_x,temp_y, temp_direction)
-            print(bounding_check(temp_x,temp_y,temp_direction), cleaning_check(temp_x, temp_y, temp_direction))
-            
-            if bounding_check(temp_x,temp_y,temp_direction) and cleaning_check(temp_x, temp_y, temp_direction):
-                x,y,direction = temp_x, temp_y, temp_direction
-                break
-            
-            num += 1
-    
-        else: # 주변에 청소가 다 되어있거나 벽이라는 뜻
-            cnt = 0
-            while bounding_check(temp_x,temp_y,direction):
-                if cnt == 0:
-                    temp_x,temp_y,temp_direction = back(x,y,direction)
-                else :
-                    temp_x,temp_y,temp_direction = back(temp_x,temp_y,temp_direction)
-                    
-                if bounding_check(temp_x,temp_y,temp_direction) and cleaning_check(temp_x, temp_y, temp_direction):
-                    x,y,direction = temp_x, temp_y, temp_direction
-                    break
-                
-                cnt += 1
-            
-            else :
-                break
-# 1번 상황 체크
-print(clean)
+    if not flag :
+        if graph[x - dx[direction]][y -dy[direction]]:
+            print(cnt)
+            break
+        else:
+            x = x - dx[direction]
+            y = y - dy[direction]
