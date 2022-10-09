5fa537c9d258a18265a487949ccd36bc4bd68294
병권 <66514138+lbg030@users.noreply.github.com>
2022-10-09 21:15:12 +0900

    1

--- testing.py
+++ testing.py
@@ -1,18 +1,1 @@
-n = int(input())
-t = []
-for i in range(n):
-    t.append(list(map(int, input().split())))
-
-print(t)
-
-for i in range(1,n):
-    for j in range(i+1):
-        if j == 0:
-            t[i][j] += t[i-1][j]
-        
-        elif i == j :
-            t[i][j] += t[i-1][j-1]
-        else:
-            t[i][j] += max(t[i - 1][j - 1], t[i - 1][j])
-            
-print(t)
+print("Hello")
