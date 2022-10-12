de726abad0a01ef4b5350bb146ced4c53a2d8c28
ByeongGwon <lee971009@naver.com>
2022-10-12 09:42:44 +0900

    1

--- testing2.py
+++ testing2.py
@@ -1,15 +1,32 @@
-import json
-annot_file_path = '1.json'
+import numpy as np
+import timm
+from tqdm import tqdm
+from skimage.transform import resize
+import os
 
-with open(annot_file_path, 'r') as f:
-		data = json.load(f)
-# print(data)
-member = data['shapes'][0]
-print(member)
-label = [member['label']]
-grid = member['points']
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-print(label, grid[0])
- 
- # 폴더 구조
-#  classification = data['classificationdata -> train[hz, bz 폴더 밑 데이터], '] // OD [data -> train[img,label]] 6 2 2
+
+model = timm.create_model('resnet50', num_classes = 4)
+
+def generate_masks(N, s, p1):
+    cell_size = np.ceil(np.array(model.input_size) / s)
+    up_size = (s + 1) * cell_size
+
+    grid = np.random.rand(N, s, s) < p1
+    grid = grid.astype('float32')
+
+    masks = np.empty((N, *model.input_size))
+
+    for i in tqdm(range(N), desc='Generating masks'):
+        # Random shifts
+        x = np.random.randint(0, cell_size[0])
+        y = np.random.randint(0, cell_size[1])
+        # Linear upsampling and cropping
+        masks[i, :, :] = resize(grid[i], up_size, order=1, mode='reflect',
+                                anti_aliasing=False)[x:x + model.input_size[0], y:y + model.input_size[1]]
+    masks = masks.reshape(-1, *model.input_size, 1)
+    return masks
+
+
+print(generate_masks(2000, 8, 0.5))
