6a0236c0962b5978745a6ec1eae7d16452b87394
ByeongGwon <lee971009@naver.com>
2022-10-20 13:16:43 +0900

    1

--- testing2.py
+++ testing2.py
@@ -1,32 +1,15 @@
-import numpy as np
-import timm
-from tqdm import tqdm
-from skimage.transform import resize
-import os
+import math
 
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+d_1 = [0.9,0.05,0.05]
+d_2 = [0.3,0.2,0.5]
 
-
-model = timm.create_model('resnet50', num_classes = 4)
-
-def generate_masks(N, s, p1):
-    cell_size = np.ceil(np.array(model.input_size) / s)
-    up_size = (s + 1) * cell_size
-
-    grid = np.random.rand(N, s, s) < p1
-    grid = grid.astype('float32')
-
-    masks = np.empty((N, *model.input_size))
-
-    for i in tqdm(range(N), desc='Generating masks'):
-        # Random shifts
-        x = np.random.randint(0, cell_size[0])
-        y = np.random.randint(0, cell_size[1])
-        # Linear upsampling and cropping
-        masks[i, :, :] = resize(grid[i], up_size, order=1, mode='reflect',
-                                anti_aliasing=False)[x:x + model.input_size[0], y:y + model.input_size[1]]
-    masks = masks.reshape(-1, *model.input_size, 1)
-    return masks
-
-
-print(generate_masks(2000, 8, 0.5))
+lst = [d_1, d_2]
+entropy = 0
+for d in lst:
+    for num in d:
+        entropy += (num * math.log(num))
+        # print(f"entropy += {num} * log{num}")
+    # print(num)
+    entropy *= -1
+    print(d,"=",entropy)
+    
