bb78d36f98dd239052bb386e1e8f8edb6dd86bb2
ByeongGwon <lee971009@naver.com>
2022-10-20 14:55:13 +0900

    1

--- testing2.py
+++ testing2.py
@@ -1,15 +1,4 @@
-import math
+NUM_TRAIN =20
+indices = list(range(NUM_TRAIN))
 
-d_1 = [0.9,0.05,0.05]
-d_2 = [0.3,0.2,0.5]
-
-lst = [d_1, d_2]
-entropy = 0
-for d in lst:
-    for num in d:
-        entropy += (num * math.log(num))
-        # print(f"entropy += {num} * log{num}")
-    # print(num)
-    entropy *= -1
-    print(d,"=",entropy)
-    
+print(indices)
