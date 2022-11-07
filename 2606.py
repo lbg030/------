784368e12be1d2f54eb4295657c1055210accb9f
병권 <66514138+lbg030@users.noreply.github.com>
2022-11-07 23:43:37 +0900

    Silver 3

--- 2606.py
+++ 2606.py
@@ -1,3 +1,20 @@
+def bfs(k):
+    q = [k]
+    res = []
+    cnt = 0
+    
+    while q:
+        number = q.pop()
+        for i in range(1,n+1):
+            if graph[number][i] == True and visited[number][i] == False:
+                q.append(i)
+                visited[number][i] = visited[i][number] = True
+        res.append(number)
+    
+    res = len(set(res)) - 1
+    return res
+
+
 n = int(input())
 m = int(input())
 
@@ -9,15 +26,7 @@ for _ in range(m):
     
     graph[a][b] = graph[b][a] = True
     
+print(bfs(1))
     
+
     
-def bfs(n):
-    q = [n]
-    # res = []
-    cnt = 0
-    
-    while q:
-        for _ in range(1,n+1):
-            pass
-        
-# print(graph)
